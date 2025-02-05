c453f4d801e751c62c62fdd64e2b1a862d4fbe16
xiezhihuan <602666178@qq.com>
2025-02-05 09:52:22 +0800

    fix(sql): 删除废弃的迁移数据

--- scripts/sql/ttpos_role_20250123.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `ttpos_role` (`id`, `name`, `order_by`, `company_id`, `create_time`, `update_time`) VALUES (2, 'Store Manager', 1, 1724054280, 1726822660, 1730346022);
-INSERT INTO `ttpos_role` (`id`, `name`, `order_by`, `company_id`, `create_time`, `update_time`) VALUES (4, 'Cashier', 1, 1724054280, 1726822660, 1736563444);
-INSERT INTO `ttpos_role` (`id`, `name`, `order_by`, `company_id`, `create_time`, `update_time`) VALUES (6, 'buyer', 1, 1724054280, 1731066434, 1731066434);
