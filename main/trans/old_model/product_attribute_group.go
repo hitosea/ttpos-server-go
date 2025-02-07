8c900f47b6d246ce112b8dfd68c1acdd54e2c538
xiezhihuan <602666178@qq.com>
2025-02-07 10:05:12 +0800

    feat(trans模块): 完成旧库的商品属性组product_attribute_group数据迁移

---
+++ main/trans/old_model/product_attribute_group.go
@@ -0,0 +1,62 @@
+package old_model
+
+import (
+	"fmt"
+	"ttpos-server-go/app/model"
+	"ttpos-server-go/app/repository/base"
+
+	"gorm.io/gorm"
+)
+
+type ProductAttributeGroup struct {
+	GroupAttributeID       uint  `gorm:"primaryKey;autoIncrement;comment:产品属性组ID"`
+	ProductID              uint  `gorm:"default:0;comment:关联产品ID"`
+	AttributeID            uint  `gorm:"default:0;comment:关联属性ID"`
+	AttributeRequired      uint  `gorm:"default:0;comment:属性组是否必填 0-否 1-是"`
+	AttributeOpenMaxSelect uint  `gorm:"default:0;comment:属性组最开启最多可选数量 0-否 1-是"`
+	AttributeMaxSelect     uint  `gorm:"default:0;comment:属性组最多可选数量"`
+	ShopSupplierID         uint  `gorm:"default:0;comment:店铺ID"`
+	AppID                  uint  `gorm:"default:0;comment:应用ID"`
+	CreateTime             int64 `gorm:"autoCreateTime;comment:创建时间"`
+	UpdateTime             int64 `gorm:"autoUpdateTime;comment:更新时间"`
+}
+
+type ProductAttributeGroupRepository interface {
+	GetProductAttributeGroupList() ([]*ProductAttributeGroup, error)
+	ConvertProductAttributeGroup() error
+}
+
+type ProductAttributeGroupService struct {
+	db       *gorm.DB
+	targetDB *gorm.DB
+}
+
+func (s *ProductAttributeGroupService) GetProductAttributeGroupList() ([]*ProductAttributeGroup, error) {
+	var productAttributeGroups []*ProductAttributeGroup
+	if err := s.db.Find(&productAttributeGroups).Error; err != nil {
+		return nil, err
+	}
+	return productAttributeGroups, nil
+}
+
+func (s *ProductAttributeGroupService) ConvertProductAttributeGroup() error {
+	var productAttributeGroups []*ProductAttributeGroup
+	if err := s.db.Find(&productAttributeGroups).Error; err != nil {
+		return err
+	}
+	for _, productAttributeGroup := range productAttributeGroups {
+		fmt.Println(fmt.Sprintf("productAttributeGroup: %+v", productAttributeGroup))
+		group := model.ProductPackageAttributeGroup{
+			Id:                 0,
+			Uuid:               productAttributeGroup.GroupAttributeID,
+			IsMust:             productAttributeGroup.AttributeRequired == 1,
+			MaxSelection:       productAttributeGroup.AttributeMaxSelect,
+			ProductPackageUuid: productAttributeGroup.ProductID,
+		}
+		_, err := base.NewProductPackageAttributeGroupRepo(s.targetDB).CreateProductPackageAttributeGroup(group)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ main/trans/old_model/product_attribute_group_test.go
@@ -0,0 +1,44 @@
+package old_model
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+	"ttpos-server-go/pkg/database"
+)
+
+func TestProductAttributeGroup(t *testing.T) {
+	db, err := NewMySQLConnection(conf, dbName)
+	if err != nil {
+		panic(err)
+	}
+	productAttributeGroupService := ProductAttributeGroupService{db: db}
+	productAttributeGroupList, err := productAttributeGroupService.GetProductAttributeGroupList()
+	if err != nil {
+		panic(err)
+	}
+	json, err := json.Marshal(productAttributeGroupList)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(json))
+}
+
+func TestConvertProductAttributeGroup(t *testing.T) {
+	database.InitSonyFlakeId()
+
+	db, err := NewMySQLConnection(conf, dbName)
+	if err != nil {
+		panic(err)
+	}
+	targetDB, err := NewMySQLConnection(targetConf, targetDBName)
+	if err != nil {
+		panic(err)
+	}
+	productAttributeGroupService := ProductAttributeGroupService{db: db, targetDB: targetDB}
+	err = productAttributeGroupService.ConvertProductAttributeGroup()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("product_attribute_group转换完成")
+}
