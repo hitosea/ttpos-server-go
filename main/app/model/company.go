f11b3e61977b796d9f74c03d1cffb8763b082b91
xiezhihuan <602666178@qq.com>
2025-02-11 23:16:43 +0800

    fix: sql所有字段名加上``避免某些名称是关键字

--- main/app/model/company.go
+++ main/app/model/company.go
@@ -19,32 +19,32 @@ type Company struct {
 
 // CompanySetting 公司设置表
 type CompanySetting struct {
-	ID                int    `gorm:"column:id;type:int(11);primary_key;AUTO_INCREMENT;comment:自增ID" json:"id"`
-	CompanyUuid       int64  `gorm:"column:company_uuid;type:bigint(20);default:0;comment:集团ID;NOT NULL" json:"company_uuid"`
-	RealName          string `gorm:"column:real_name;type:varchar(50);comment:真实姓名;NOT NULL" json:"real_name"`
-	LinkName          string `gorm:"column:link_name;type:varchar(50);comment:联系人;NOT NULL" json:"link_name"`
-	LinkPhone         string `gorm:"column:link_phone;type:varchar(25);comment:联系电话;NOT NULL" json:"link_phone"`
-	SaleStock         int    `gorm:"column:sale_stock;type:int(11);default:0;comment:进销存: 0不开启, 1开启;NOT NULL" json:"sale_stock"`
-	IsOpenMember      int    `gorm:"column:is_open_member;type:int(11);default:0;comment:是否开启会员: 0不开启, 1开启;NOT NULL" json:"is_open_member"`
-	IsOpenTablet      int    `gorm:"column:is_open_tablet;type:int(11);default:0;comment:是否开启平板: 0不开启, 1开启;NOT NULL" json:"is_open_tablet"`
-	IsOpenScan        int    `gorm:"column:is_open_scan;type:int(11);default:0;comment:是否开启扫码H5: 0不开启, 1开启;NOT NULL" json:"is_open_scan"`
-	IsOpenAssistant   int    `gorm:"column:is_open_assistant;type:int(11);default:0;comment:是否开启点餐助手: 0不开启, 1开启;NOT NULL" json:"is_open_assistant"`
-	IsOpenKitchenKds  int    `gorm:"column:is_open_kitchen_kds;type:int(11);default:0;comment:是否开启后厨KDS: 0不开启, 1开启;NOT NULL" json:"is_open_kitchen_kds"`
-	IsOpenBuffet      int    `gorm:"column:is_open_buffet;type:int(11);default:0;comment:是否开启自助餐: 0不开启, 1开启;NOT NULL" json:"is_open_buffet"`
-	IsAcceptScanOrder int    `gorm:"column:is_accept_scan_order;type:int(11);default:0;comment:是否开启扫码点餐接单 0不开启, 1开启;NOT NULL" json:"is_accept_scan_order"`
-	IsOpenLocalPrint  int    `gorm:"column:is_open_local_print;type:int(11);default:1;comment:是否开启本地打印服务 0不开启, 1开启;NOT NULL" json:"is_open_local_print"`
-	CashLimit         int    `gorm:"column:cash_limit;type:int(11);default:0;comment:收银机上限;NOT NULL" json:"cash_limit"`
-	KitchenLimit      int    `gorm:"column:kitchen_limit;type:int(11);default:0;comment:厨显上限;NOT NULL" json:"kitchen_limit"`
-	TabletLimit       int    `gorm:"column:tablet_limit;type:int(11);default:0;comment:平板上限;NOT NULL" json:"tablet_limit"`
-	AssistantLimit    int    `gorm:"column:assistant_limit;type:int(11);default:0;comment:点餐助手上限;NOT NULL" json:"assistant_limit"`
-	TableLimit        int    `gorm:"column:table_limit;type:int(11);default:0;comment:桌台上限;NOT NULL" json:"table_limit"`
-	PrinterLimit      int    `gorm:"column:printer_limit;type:int(11);default:0;comment:打印机上限;NOT NULL" json:"printer_limit"`
-	Timezone          string `gorm:"column:timezone;type:varchar(50);default:Asia/Shanghai;comment:时区;NOT NULL" json:"timezone"`
-	Languages         string `gorm:"column:languages;type:varchar(255);comment:支持语言;NOT NULL" json:"languages"`
-	Address           string `gorm:"column:address;type:varchar(255);comment:联系地址;NOT NULL" json:"address"`
-	CreateTime        int    `gorm:"autoCreateTime;column:create_time;type:int(10);comment:创建时间（时间戳）;NOT NULL" json:"create_time"`
-	UpdateTime        int    `gorm:"autoUpdateTime;column:update_time;type:int(10);comment:更新时间（时间戳）;NOT NULL" json:"update_time"`
-	DeleteTime        int    `gorm:"column:delete_time;type:int(10);default:0;comment:删除时间（时间戳）;NOT NULL" json:"delete_time"`
+	ID               int    `gorm:"column:id;type:int(11);primary_key;AUTO_INCREMENT;comment:自增ID" json:"id"`
+	CompanyUuid      int64  `gorm:"column:company_uuid;type:bigint(20);default:0;comment:集团ID;NOT NULL" json:"company_uuid"`
+	RealName         string `gorm:"column:real_name;type:varchar(50);comment:真实姓名;NOT NULL" json:"real_name"`
+	LinkName         string `gorm:"column:link_name;type:varchar(50);comment:联系人;NOT NULL" json:"link_name"`
+	LinkPhone        string `gorm:"column:link_phone;type:varchar(25);comment:联系电话;NOT NULL" json:"link_phone"`
+	SaleStock        int    `gorm:"column:sale_stock;type:int(11);default:0;comment:进销存: 0不开启, 1开启;NOT NULL" json:"sale_stock"`
+	IsOpenMember     int    `gorm:"column:is_open_member;type:int(11);default:0;comment:是否开启会员: 0不开启, 1开启;NOT NULL" json:"is_open_member"`
+	IsOpenTablet     int    `gorm:"column:is_open_tablet;type:int(11);default:0;comment:是否开启平板: 0不开启, 1开启;NOT NULL" json:"is_open_tablet"`
+	IsOpenScan       int    `gorm:"column:is_open_scan;type:int(11);default:0;comment:是否开启扫码H5: 0不开启, 1开启;NOT NULL" json:"is_open_scan"`
+	IsOpenAssistant  int    `gorm:"column:is_open_assistant;type:int(11);default:0;comment:是否开启点餐助手: 0不开启, 1开启;NOT NULL" json:"is_open_assistant"`
+	IsOpenKitchenKds int    `gorm:"column:is_open_kitchen_kds;type:int(11);default:0;comment:是否开启后厨KDS: 0不开启, 1开启;NOT NULL" json:"is_open_kitchen_kds"`
+	IsOpenBuffet     int    `gorm:"column:is_open_buffet;type:int(11);default:0;comment:是否开启自助餐: 0不开启, 1开启;NOT NULL" json:"is_open_buffet"`
+	IsOpenScanOrder  int    `gorm:"column:is_open_scan_order;type:int(11);default:0;comment:是否开启扫码点餐接单 0不开启, 1开启;NOT NULL" json:"is_open_scan_order"`
+	IsOpenLocalPrint int    `gorm:"column:is_open_local_print;type:int(11);default:1;comment:是否开启本地打印服务 0不开启, 1开启;NOT NULL" json:"is_open_local_print"`
+	CashLimit        int    `gorm:"column:cash_limit;type:int(11);default:0;comment:收银机上限;NOT NULL" json:"cash_limit"`
+	KitchenLimit     int    `gorm:"column:kitchen_limit;type:int(11);default:0;comment:厨显上限;NOT NULL" json:"kitchen_limit"`
+	TabletLimit      int    `gorm:"column:tablet_limit;type:int(11);default:0;comment:平板上限;NOT NULL" json:"tablet_limit"`
+	AssistantLimit   int    `gorm:"column:assistant_limit;type:int(11);default:0;comment:点餐助手上限;NOT NULL" json:"assistant_limit"`
+	TableLimit       int    `gorm:"column:table_limit;type:int(11);default:0;comment:桌台上限;NOT NULL" json:"table_limit"`
+	PrinterLimit     int    `gorm:"column:printer_limit;type:int(11);default:0;comment:打印机上限;NOT NULL" json:"printer_limit"`
+	Timezone         string `gorm:"column:timezone;type:varchar(50);default:Asia/Shanghai;comment:时区;NOT NULL" json:"timezone"`
+	Languages        string `gorm:"column:languages;type:varchar(255);comment:支持语言;NOT NULL" json:"languages"`
+	Address          string `gorm:"column:address;type:varchar(255);comment:联系地址;NOT NULL" json:"address"`
+	CreateTime       int    `gorm:"autoCreateTime;column:create_time;type:int(10);comment:创建时间（时间戳）;NOT NULL" json:"create_time"`
+	UpdateTime       int    `gorm:"autoUpdateTime;column:update_time;type:int(10);comment:更新时间（时间戳）;NOT NULL" json:"update_time"`
+	DeleteTime       int    `gorm:"column:delete_time;type:int(10);default:0;comment:删除时间（时间戳）;NOT NULL" json:"delete_time"`
 }
 
 // CompanyStaff saas库保存的集团员工关联表
--- main/app/model/rbac.go
+++ main/app/model/rbac.go
@@ -24,24 +24,24 @@ type Access struct {
 
 // Staff 员工表
 type Staff struct {
-	ID               uint   `gorm:"column:id;type:int(11) unsigned;AUTO_INCREMENT;primary_key;comment:自增ID" json:"id"`
-	Uuid             uint64 `gorm:"column:uuid;type:bigint(20) unsigned;default:0;comment:员工ID;NOT NULL" json:"uuid"`
-	CompanyUuid      uint64 `gorm:"column:company_uuid;type:bigint(20) unsigned;default:0;comment:集团ID;NOT NULL" json:"company_uuid"`
-	Username         string `gorm:"column:username;type:varchar(255);comment:用户名;NOT NULL" json:"username"`
-	Password         string `gorm:"column:password;type:varchar(255);comment:登录密码;NOT NULL" json:"password"`
-	Phone            string `gorm:"column:phone;type:varchar(20);comment:手机号" json:"phone"`
-	PasswordChange   int    `gorm:"column:password_change;type:int(11);default:0;comment:修改密码次数" json:"password_change"`
-	RealName         string `gorm:"column:real_name;type:varchar(255);comment:姓名;NOT NULL" json:"real_name"`
-	IsSuper          int    `gorm:"column:is_super;type:tinyint(3);default:0;comment:是否为超级管理员0不是,1是;NOT NULL" json:"is_super"`
-	UserType         int    `gorm:"column:user_type;type:tinyint(1);default:0;comment:账号类型0总台1门店;NOT NULL" json:"user_type"`
-	IsDisable        int    `gorm:"column:is_disable;type:tinyint(3);default:0;comment:是否禁用1禁用，0未禁用;NOT NULL" json:"is_disable"`
-	BindKey          string `gorm:"column:bind_key;type:varchar(255);comment:绑定的设备key" json:"bind_key"`
-	CashierOnline    int    `gorm:"column:cashier_online;type:tinyint(1);default:0;comment:收银员当班 0-不在线 1-在线;NOT NULL" json:"cashier_online"`
-	CashierLoginTime int    `gorm:"column:cashier_login_time;type:int(11);default:0;comment:收银员当班登录时间;NOT NULL" json:"cashier_login_time"`
-	DutyNo           string `gorm:"column:duty_no;type:varchar(64);comment:当班编号" json:"duty_no"`
-	CreateTime       int    `gorm:"autoCreateTime;column:create_time;type:int(10);comment:创建时间(时间戳);NOT NULL" json:"create_time"`
-	UpdateTime       int    `gorm:"autoUpdateTime;column:update_time;type:int(10);comment:更新时间(时间戳);NOT NULL" json:"update_time"`
-	DeleteTime       int    `gorm:"column:delete_time;type:int(10);default:0;comment:删除时间(时间戳);NOT NULL" json:"delete_time"`
+	ID                  uint   `gorm:"column:id;type:int(11) unsigned;AUTO_INCREMENT;primary_key;comment:自增ID" json:"id"`
+	Uuid                uint64 `gorm:"column:uuid;type:bigint(20) unsigned;default:0;comment:员工ID;NOT NULL" json:"uuid"`
+	CompanyUuid         uint64 `gorm:"column:company_uuid;type:bigint(20) unsigned;default:0;comment:集团ID;NOT NULL" json:"company_uuid"`
+	Username            string `gorm:"column:username;type:varchar(255);comment:用户名;NOT NULL" json:"username"`
+	Password            string `gorm:"column:password;type:varchar(255);comment:登录密码;NOT NULL" json:"password"`
+	Phone               string `gorm:"column:phone;type:varchar(20);comment:手机号" json:"phone"`
+	PasswordChangeCount int    `gorm:"column:password_change_count;type:int(11);default:0;comment:修改密码次数" json:"password_change"`
+	RealName            string `gorm:"column:real_name;type:varchar(255);comment:姓名;NOT NULL" json:"real_name"`
+	IsSuper             int    `gorm:"column:is_super;type:tinyint(3);default:0;comment:是否为超级管理员0不是,1是;NOT NULL" json:"is_super"`
+	UserType            int    `gorm:"column:user_type;type:tinyint(1);default:0;comment:账号类型0总台1门店;NOT NULL" json:"user_type"`
+	IsDisable           int    `gorm:"column:is_disable;type:tinyint(3);default:0;comment:是否禁用1禁用，0未禁用;NOT NULL" json:"is_disable"`
+	BindKey             string `gorm:"column:bind_key;type:varchar(255);comment:绑定的设备key" json:"bind_key"`
+	CashierOnline       int    `gorm:"column:cashier_online;type:tinyint(1);default:0;comment:收银员当班 0-不在线 1-在线;NOT NULL" json:"cashier_online"`
+	CashierLoginTime    int    `gorm:"column:cashier_login_time;type:int(11);default:0;comment:收银员当班登录时间;NOT NULL" json:"cashier_login_time"`
+	DutyNo              string `gorm:"column:duty_no;type:varchar(64);comment:当班编号" json:"duty_no"`
+	CreateTime          int    `gorm:"autoCreateTime;column:create_time;type:int(10);comment:创建时间(时间戳);NOT NULL" json:"create_time"`
+	UpdateTime          int    `gorm:"autoUpdateTime;column:update_time;type:int(10);comment:更新时间(时间戳);NOT NULL" json:"update_time"`
+	DeleteTime          int    `gorm:"column:delete_time;type:int(10);default:0;comment:删除时间(时间戳);NOT NULL" json:"delete_time"`
 
 	Company *Company `gorm:"foreignKey:company_uuid;references:Uuid"`
 }
