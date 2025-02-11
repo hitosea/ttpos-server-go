f8ede67c7239923668d1a8783a0209fc2eea7ee6
xiezhihuan <602666178@qq.com>
2025-02-11 22:38:46 +0800

    feat: 添加时间工具类，指定时区后获取现在时间、今天时间区间、昨天时间区间、本周时间区间

---
+++ main/pkg/utils/time.go
@@ -0,0 +1,67 @@
+package utils
+
+import "time"
+
+type TimeUtil interface {
+	Now() time.Time                            // 获取当前时间
+	NowUnix() int64                            // 获取当前时间戳,10位，1739283862
+	NowUnixMilli() int64                       // 获取当前时间戳（毫秒）13位，1739283862825
+	NowUnixMicro() int64                       // 获取当前时间戳（微秒）,16位，1739283862825531
+	TodayStartEnd() (time.Time, time.Time)     // 获取今天的开始时间和结束时间
+	YesterdayStartEnd() (time.Time, time.Time) // 获取昨天的开始时间和结束时间
+	WeekStartEnd() (time.Time, time.Time)      // 获取本周的开始时间和结束时间
+}
+
+type Timezone string
+
+const (
+	ZH_TIMEZONE Timezone = "Asia/Shanghai"   // 中国时区 UTC+8
+	JP_TIMEZONE Timezone = "Asia/Tokyo"      // 日本时区 UTC+9
+	TH_TIMEZONE Timezone = "Asia/Bangkok"    // 泰国时区 UTC+7
+	TR_TIMEZONE Timezone = "Europe/Istanbul" // 土耳其时区 UTC+3
+)
+
+func (t Timezone) Now() time.Time {
+	loc, err := time.LoadLocation(string(t))
+	if err != nil {
+		return time.Now()
+	}
+	return time.Now().In(loc)
+}
+
+func (t Timezone) NowUnix() int64 {
+	return t.Now().Unix()
+}
+
+func (t Timezone) NowUnixMilli() int64 {
+	return t.Now().UnixMilli()
+}
+
+func (t Timezone) NowUnixMicro() int64 {
+	return t.Now().UnixMicro()
+}
+
+func (t Timezone) TodayStartEnd() (time.Time, time.Time) {
+	now := t.Now()
+	start := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+	end := time.Date(now.Year(), now.Month(), now.Day(), 23, 59, 59, 999999999, now.Location())
+	return start, end
+}
+
+func (t Timezone) YesterdayStartEnd() (time.Time, time.Time) {
+	now := t.Now().AddDate(0, 0, -1)
+	start := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+	end := time.Date(now.Year(), now.Month(), now.Day(), 23, 59, 59, 999999999, now.Location())
+	return start, end
+}
+
+func (t Timezone) WeekStartEnd() (time.Time, time.Time) {
+	now := t.Now()
+	weekday := int(now.Weekday())
+	if weekday == 0 {
+		weekday = 7
+	}
+	start := time.Date(now.Year(), now.Month(), now.Day()-weekday+1, 0, 0, 0, 0, now.Location())
+	end := time.Date(now.Year(), now.Month(), now.Day()+(7-weekday), 23, 59, 59, 999999999, now.Location())
+	return start, end
+}
---
+++ main/pkg/utils/time_test.go
@@ -0,0 +1,23 @@
+package utils
+
+import "testing"
+
+func TestTimezone_Now(t *testing.T) {
+	//tz := Timezone(ZH_TIMEZONE)
+	tz := Timezone("Asia/Shanghai")
+	t.Log(tz.Now())
+	t.Log(tz.NowUnix())
+	t.Log(tz.NowUnixMilli())
+	t.Log(tz.NowUnixMicro())
+	t.Log(tz.TodayStartEnd())
+	t.Log(tz.YesterdayStartEnd())
+	t.Log(tz.WeekStartEnd())
+	jp := Timezone(JP_TIMEZONE)
+	t.Log(jp.Now())
+	t.Log(jp.NowUnix())
+	t.Log(jp.NowUnixMilli())
+	t.Log(jp.NowUnixMicro())
+	t.Log(jp.TodayStartEnd())
+	t.Log(jp.YesterdayStartEnd())
+	t.Log(jp.WeekStartEnd())
+}
