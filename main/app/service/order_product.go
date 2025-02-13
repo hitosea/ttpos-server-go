9f574fbebeec0454e925018c604d4bfce98afbc7
王昱 <nightcp874@gmail.com>
2025-02-13 15:50:17 +0800

    feat: 定义商品订单服务接口方法

--- main/app/service/order_product.go
+++ main/app/service/order_product.go
@@ -1,9 +1,18 @@
 package service
 
-import "ttpos-server-go/pkg/database"
+import (
+	"ttpos-server-go/app/model"
+	"ttpos-server-go/pkg/database"
+)
 
 // IOrderProductSrv 定义订单商品服务接口
-type IOrderProductSrv interface{}
+type IOrderProductSrv interface {
+	CheckProductOrderFlavor(productPackage model.ProductPackage, flavorUuid uint64) error                      // 检查商品规格
+	CheckProductOrderSauce(productPackage model.ProductPackage, sauceUuids []uint64) error                     // 检查商品加料
+	CheckProductOrderAttribute(productPackage model.ProductPackage, attributes []model.ProductAttribute) error // 检查商品属性
+	CheckProductOrderFlavorStock(productPackage model.ProductPackage, sauceUuids []uint64) error               // 检查商品规格库存
+	CheckProductOrderSauceStock(productPackage model.ProductPackage, sauceUuids []uint64) error                // 检查商品加料库存
+}
 
 // orderProductSrv 订单商品服务结构体
 type orderProductSrv struct {
@@ -20,4 +29,29 @@ func NewOrderProductSrvImpl(dbm *database.DBManager) IOrderProductSrv {
 	return &orderProductSrv{
 		dbm: dbm,
 	}
+}
+
+func (o orderProductSrv) CheckProductOrderFlavor(productPackage model.ProductPackage, flavorUuid uint64) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (o orderProductSrv) CheckProductOrderSauce(productPackage model.ProductPackage, sauceUuids []uint64) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (o orderProductSrv) CheckProductOrderAttribute(productPackage model.ProductPackage, attributes []model.ProductAttribute) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (o orderProductSrv) CheckProductOrderFlavorStock(productPackage model.ProductPackage, sauceUuids []uint64) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (o orderProductSrv) CheckProductOrderSauceStock(productPackage model.ProductPackage, sauceUuids []uint64) error {
+	//TODO implement me
+	panic("implement me")
 }
