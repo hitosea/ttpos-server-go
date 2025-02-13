fb3af249e1f75d7a3f5c64ed8fe66a15d9f9c004
xiezhihuan <602666178@qq.com>
2025-02-14 02:25:47 +0800

    fix: ttpos_sale_order_product修改金额整个计算相关字段

--- admin/database/seeds/shop_01.sql
+++ admin/database/seeds/shop_01.sql
@@ -138,31 +138,56 @@ CREATE TABLE IF NOT EXISTS `ttpos_qrcode_order` (
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '扫码(接单)订单表';
 
 CREATE TABLE IF NOT EXISTS `ttpos_sale_order_product` (
+    -- 基本信息
     `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '自增ID',
     `uuid` BIGINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '销售订单商品ID',
     `name` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '商品名称',
     `flavor_name` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '规格名称',
     `multi_language_name_uuid` BIGINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '多语言名称ID',
-    `num` INT(11) NOT NULL DEFAULT 0 COMMENT '商品数量',
+    `num` INT(11) NOT NULL DEFAULT 0 COMMENT '商品数量。不能减为0，当数量为1再减时，标记删除',
     `image_file_uuid` BIGINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '商品图片ID',
-    `custom_price` DECIMAL(12, 2) NOT NULL DEFAULT 0 COMMENT '自定义价格',
-    `unit_price` DECIMAL(12, 2) NOT NULL DEFAULT 0 COMMENT '单价,商品原价+小料价',
-    `price` DECIMAL(12, 2) NOT NULL DEFAULT 0 COMMENT '最终单价(折扣和优惠后)',
-    `service_fee` DECIMAL(12, 2) NOT NULL DEFAULT 0 COMMENT '服务费,按比例收取时有值',
-    `tax_rate` TINYINT(1) NOT NULL DEFAULT 0 COMMENT '税率,单位%.下单时单税率,结账时再重新核算',
-    `tax_fee` DECIMAL(12, 2) NOT NULL DEFAULT 0 COMMENT '税费',
-    `product_original_amount` DECIMAL(12, 2) NOT NULL DEFAULT 0 COMMENT '原价销售额.包含加料、税费.',
-    `status` TINYINT(1) NOT NULL DEFAULT 0 COMMENT '状态, 0-未送厨 1-已送厨 2-已退菜',
-    `is_require` TINYINT(1) NOT NULL DEFAULT 0 COMMENT '是否必点商品 0-否 1-是',
-    `deduct_stock_type` TINYINT(3) NOT NULL DEFAULT 0 COMMENT '库存计算方式,0-下单减库存 1-付款减库存。创建商品后不变',
-    `remark` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '备注',
-    `is_gift` TINYINT(1) NOT NULL DEFAULT 0 COMMENT '是否赠品, 0-否 1-是',
-    `gift_reason` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '赠品原因',
-    `sign` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '商品签名,规格属性加料相同的商品签名相同,用于取消拆单时合并商品',
+    -- 价格信息
+    `flavor_price` DECIMAL(12, 2) NOT NULL DEFAULT 0 COMMENT '规格原价,仅某规格商品的原价',
+    `sauce_price` DECIMAL(12, 2) NOT NULL DEFAULT 0 COMMENT '小料价,所有小料的价格之和',
+    `product_price` DECIMAL(12, 2) NOT NULL DEFAULT 0 COMMENT '原始单价,规格原价+小料价',
+    `is_custom_price` TINYINT(1) NOT NULL DEFAULT 0 COMMENT '是否自定义价格, 0-否 1-是',
+    `sale_price` DECIMAL(12, 2) NOT NULL DEFAULT 0 COMMENT '销售价,当自定义价格时，销售价=自定义价格,否则销售价=原始单价',
+    -- 税率
+    `tax_rate` TINYINT(1) NOT NULL DEFAULT 0 COMMENT '税率,单位%.加购时记录税率,结账时再重新核算',
+    -- 折扣率=会员折扣率*会员卡折扣率*自定义折扣率
+    `member_discount_rate` DECIMAL(12, 2) NOT NULL DEFAULT 0 COMMENT '会员折扣率(0-100%)',
+    `member_card_discount_rate` DECIMAL(12, 2) NOT NULL DEFAULT 0 COMMENT '会员卡折扣率(0-100%)',
+    `custom_discount_rate` DECIMAL(12, 2) NOT NULL DEFAULT 0 COMMENT '自定义折扣率(0-100%)',
+    -- 最终单价=销售价*折扣率
+    `price` DECIMAL(12, 2) NOT NULL DEFAULT 0 COMMENT '最终单价(会员、会员卡和优惠折扣后)。销售价*折扣率',
+     -- 总税费=商品税费+服务费税费
+    `service_tax_fee` DECIMAL(12, 2) NOT NULL DEFAULT 0 COMMENT '服务费税费,0-不收取税费；收取时，服务费税费=服务费*税率',
+    `tax_fee` DECIMAL(12, 2) NOT NULL DEFAULT 0 COMMENT '商品税费。商品已含税时，税费=规格原价*(1-1/(1+税率))；商品未含税时，税费=原始单价*税率',
+    -- 服务费
+    `service_fee` DECIMAL(12, 2) NOT NULL DEFAULT 0 COMMENT '服务费,0-固定服务费 大于0-按比例收服务费；商品已含税时，服务费=(最终单价-商品税费)*服务费比例；商品未含税时，服务费=最终单价*服务费比例',
+   -- 单个商品应收金额=最终单价+服务费+总税费
+    `total_price` DECIMAL(12, 2) NOT NULL DEFAULT 0 COMMENT '应收金额(单商品)=最终单价+服务费+总税费',
+    -- 状态值
+    `status` TINYINT(1) NOT NULL DEFAULT 0 COMMENT '状态, 0-未送厨 1-已送厨 2-已退',
+    `is_require` TINYINT(1) NOT NULL DEFAULT 0 COMMENT '是否必点商品 0-否 1-是。用于在前端显示必点图标',
+    -- 下单是指加购商品吗？还是送厨商品？如果下单指加购，则可以理解这类商品为抢购商品，先抢先得。
+    `deduct_stock_type` TINYINT(3) NOT NULL DEFAULT 0 COMMENT '库存计算方式,0-下单减库存 1-付款减库存。加购商品时记录，不受后台影响，用于减少查询次数',
+    -- 送厨时检查商品是否要减库存；结账时检查商品是否已减库存，无论商品是下单减库存还是付款减库存，都要检查商品是否已减库存，避免商品漏减库存
+    `deduct_stock_time` INT(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '减库存的时间(时间戳)，0-未减库存。标记是否已减库存，用于取消订单时恢复库存、避免重复减库存、避免漏减库存',
+    `remark` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '备注，顾客对商品的备注信息',
+    `is_gift` TINYINT(1) NOT NULL DEFAULT 0 COMMENT '是否赠菜, 0-否 1-是',
+    `gift_reason` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '赠菜原因',
+    `sign` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '商品签名,规格、属性、加料、是否改价、是否赠菜、送厨批次、销售价相同的商品签名相同,用于取消拆单时合并商品',
+    -- 关联信息
+    `production_order_uuid` BIGINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '生产订单ID',
     `product_package_uuid` BIGINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '商品包ID',
     `sale_bill_uuid` BIGINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '销售账单ID',
     `sale_order_uuid` BIGINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '销售订单ID',
-    `qrcode_order_uuid` BIGINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '扫码订单ID',
+    -- 扫码订单相关  // todo 设计新表，扫码订单商品 ttpos_h5_order_product
+    `qrcode_order_uuid` BIGINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '扫码订单ID，用于关联扫码订单，用于判断是否为扫码订单商品',
+    `is_qrcode_order_product` TINYINT(1) NOT NULL DEFAULT 0 COMMENT '是否为扫码订单商品, 0-否 1-是',
+    `is_accept_order` TINYINT(1) NOT NULL DEFAULT 0 COMMENT '是否已接单, 0-否 1-是',
+    -- 时间信息
     `create_time` INT(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间(时间戳)',
     `update_time` INT(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '更新时间(时间戳)',
     `delete_time` INT(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '删除时间(时间戳)',
