c10d5671a87c8dbcba5328b6a67c10985b6a1c67
wfs <605403358@qq.com>
2025-02-17 10:46:54 +0800

    build: 编译资源上传

--- admin/public/shop/config.js
+++ admin/public/shop/config.js
@@ -1,7 +1,7 @@
 window.config = () => {
   return {
     title: 'TTPOS', // 默认浏览器标题 JBCレジ, TTPOS
-    brand: 'ttpos', // 地区 日本=> jbc, 泰国=>TTPOS
+    brand: 'TTPOS', // 地区 日本=> jbc, 泰国=>TTPOS
     webLogo: '', // webLogo有值是优先加载Logo 否则按照地区加载默认图标
     build: 76, // 版本build
     versionName: '1.1.0', // 版本号
