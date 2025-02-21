a19d0624ab9beec1a2a0eacbac14e6dd49f3e970
王昱 <nightcp874@gmail.com>
2025-02-21 11:08:01 +0800

    fix: 修复商家后台无法修改商品分类
    分类列表category_id、parent_id返回字符串类型

--- admin/app/shop/controller/product/store/Category.php
+++ admin/app/shop/controller/product/store/Category.php
@@ -5,6 +5,7 @@ namespace app\shop\controller\product\store;
 use app\shop\controller\Controller;
 use hg\apidoc\annotation as Apidoc;
 use app\shop\model\product\Category as CategoryModel;
+use think\facade\Log;
 
 /**
  * 店内商品分类
@@ -27,6 +28,7 @@ class Category extends Controller
         $model = new CategoryModel;
         $name = isset($params['name']) ? $params['name'] : '';
         $list = $model->getCacheAll(1, 0, $this->store, $name, false) ?: [];
+        Log::info(json_encode($list));
         // 列表关联产品数量
         foreach ($list['data'] as &$item) {
             // 处理按钮子分类
