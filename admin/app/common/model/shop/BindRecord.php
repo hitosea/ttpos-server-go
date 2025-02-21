cc80e8b0c42665dd8e52277579058daedc06676a
王昱 <nightcp874@gmail.com>
2025-02-21 14:44:06 +0800

    fix: 修复收银机设置-登录过的设备显示错误

--- admin/app/common/model/shop/BindRecord.php
+++ admin/app/common/model/shop/BindRecord.php
@@ -235,6 +235,7 @@ class BindRecord extends BaseModel
             if ($item['source'] == self::SOURCE_CASHIER) {
                 $item['is_cashier_shift'] = 1;
                 $item['key'] = $item['device_id'];
+                $item['finally_login_id'] = $item['finally_login_uuid'];
                 $user = User::where(['bind_key' => $item['key']])->find();
                 if ($user) {
                     $item['is_cashier_shift'] = $user->cashier_online ? 0 : 1;
