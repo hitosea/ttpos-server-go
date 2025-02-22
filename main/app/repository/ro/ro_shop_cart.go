7c3b75f2d1a359fdc534938066bc6cc9c208b9c7
wfs <605403358@qq.com>
2025-02-22 21:25:44 +0800

    feat:  强化判断

--- main/app/repository/ro/ro_shop_cart.go
+++ main/app/repository/ro/ro_shop_cart.go
@@ -7,5 +7,11 @@ type ShopCartRepo struct {
 }
 
 func (ro *ShopCartRepo) IsDeskShopCart() bool {
+	if ro.SaleBill == nil {
+		return false
+	}
+	if ro.SaleBill.Desk == nil {
+		return false
+	}
 	return ro.SaleBill.Desk.Uuid != 0 // 如果是桌台购物车的话肯定会查询有桌台信息
 }
