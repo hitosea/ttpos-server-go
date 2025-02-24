50028436913731016d5833ffb2520f71dd6328fd
wfs <605403358@qq.com>
2025-02-24 22:05:54 +0800

    feat:  添加迁移文件

--- admin/database/migrations/20250224122522_add_is_h5_order_product_to_sale_order_product.php
+++ admin/database/migrations/20250224122522_add_is_h5_order_product_to_sale_order_product.php
@@ -29,6 +29,14 @@ class addIsH5OrderProductToSaleOrderProduct extends Migrator
     public function change()
     {
         $table = $this->table('sale_order_product');
+        if (!$table->hasColumn('h5_order_uuid')) {
+            $table->addColumn(Column::bigInteger('h5_order_uuid')->setDefault(0)->setComment('扫码订单ID，用于关联扫码订单，用于判断是否为扫码订单商品')->setAfter('sale_order_uuid'));
+            $table->update();
+        }
+        if (!$table->hasColumn('h5_order_product_uuid')) {
+            $table->addColumn(Column::bigInteger('h5_order_product_uuid')->setDefault(0)->setComment('h5订单商品ID，用于关联h5订单商品，用于判断是否为h5订单商品')->setAfter('h5_order_uuid'));
+            $table->update();
+        }
         if (!$table->hasColumn('is_h5_order_product')) {
             $table->addColumn(Column::tinyInteger('is_h5_order_product')->setDefault(0)->setComment('是否为扫码订单商品, 0-否 1-是')->setAfter('h5_order_product_uuid'));
             $table->update();
