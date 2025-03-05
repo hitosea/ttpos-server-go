c6cfc7954b581538908a91694a15dc8737a249d8
xiezhihuan <602666178@qq.com>
2025-03-06 06:58:32 +0800

    fix: 修复点餐购物车

--- main/app/model/order.go
+++ main/app/model/order.go
@@ -23,7 +23,7 @@ type SaleBill struct {
 	SerialNo string `gorm:"column:serial_no;type:varchar(255);default:'';comment:桌位编号 (点餐流水号)" json:"serial_no"`
 
 	// 状态相关字段
-	Status uint `gorm:"column:status;type:tinyint(1);default:3;comment:订单状态, 0-待付款、1-已完成、2-已取消、3-开单未送厨，这个账单的初始状态。订单列表中只显示0 1 2状态的订单" json:"status"`
+	Status uint `gorm:"column:status;type:tinyint(1);default:0;comment:订单状态, 0-待付款、1-已完成、2-已取消" json:"status"`
 	IsLock uint `gorm:"column:is_lock;type:tinyint(1);default:0;comment:是否锁单, 0-否 1-是" json:"is_lock"`
 
 	// 订单类型字段
