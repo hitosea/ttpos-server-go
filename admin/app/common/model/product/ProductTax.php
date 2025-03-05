c13ed138c9e3169fe69bce5d2af7a162e540060c
王昱 <nightcp874@gmail.com>
2025-03-05 20:10:18 +0800

    feat(shop): 如果开启后之前没有过税类的商品、自助餐默认选择第一个税类

--- admin/app/common/model/product/ProductTax.php
+++ admin/app/common/model/product/ProductTax.php
@@ -3,6 +3,7 @@
 namespace app\common\model\product;
 
 use app\common\model\BaseModel;
+use app\common\model\buffet\Buffet;
 use app\common\model\tax\TaxCategory;
 
 /**
@@ -68,38 +69,37 @@ class ProductTax extends BaseModel
      */
     public function getProductDefaultTaxCategory()
     {
-        // 查询所有商品成品
-        $product = new Product();
-        $productList = $product->where('type', Product::TYPE_PRODUCT)->select();
-        // 如果没有商品成品，直接返回
-        if (count($productList) == 0) {
-            return;
-        }
         // 查询第一个税类
         $taxCategory = TaxCategory::order('id asc')->find();
-        // 遍历商品成品，找到没有过税类的商品
         $productTax = [];
-        foreach ($productList as $product) {
-            $existingTax = $this->where('product_id', $product['product_id'])->where('tax_category_id', 0)->find();
-            if ($existingTax) {
-                $this->where('product_id', $product['product_id'])->update(['tax_category_id' => $taxCategory['id']]);
-                continue;
-            }
-            if ($this->where('product_id', $product['product_id'])->where('tax_category_id', '>', 0)->count()) {
-                continue;
-            }
-            // 如果没有过税类，则选择第一个税类
+        $buffetTax = [];
+
+        // 查询没有税类的商品
+        $product = new Product();
+        $productList = $product->where('dine_tax_uuid', 0)->whereOr('takeout_tax_uuid', 0)->select();        
+        foreach ($productList as $productItem) {
             $productTax[] = [
-                'product_id' => $product['product_id'],
-                'product_tax_type' => 1, //产品税率类型，1-堂食税类，2-外带税类
-                'tax_category_id' => $taxCategory['id'],
-            ];
-            $productTax[] = [
-                'product_id' => $product['product_id'],
-                'product_tax_type' => 2, //产品税率类型，1-堂食税类，2-外带税类
-                'tax_category_id' => $taxCategory['id'],
+                'id' => $productItem['id'],
+                'dine_tax_uuid' => $taxCategory['uuid'],
+                'takeout_tax_uuid' => $taxCategory['uuid'],
             ];
         }
-        $this->saveAll($productTax);
+
+        // 查询没有税类的自助餐
+        $buffet = new Buffet();
+        $buffetList = $buffet->where('tax_uuid', 0)->select();
+        foreach ($buffetList as $buffetItem) {
+            $buffetTax[] = [
+                'id' => $buffetItem->getData('id'),
+                'tax_uuid' => $taxCategory['uuid'],
+            ];
+        }
+
+        if (!empty($productTax)) {
+            $product->saveAll($productTax);
+        }
+        if (!empty($buffetTax)) {
+            $buffet->saveAll($buffetTax);
+        }
     }
 }
