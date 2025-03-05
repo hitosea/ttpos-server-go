05b17f31bc1f89195143bf48228e2acfeba64fb6
王昱 <nightcp874@gmail.com>
2025-03-05 21:30:52 +0800

    fix: 修复执行return_order_amount迁移文件失败

--- admin/database/migrations/20250305125858_create_table_return_order_amount.php
+++ admin/database/migrations/20250305125858_create_table_return_order_amount.php
@@ -36,19 +36,21 @@ class CreateTableReturnOrderAmount extends Migrator
             'comment' => '退款金额表'
         ]);
 
-        $table->addColumn('id', 'integer', ['signed' => false, 'identity' => true, 'comment' => '自增ID'])
-            ->addColumn('uuid', 'biginteger', ['signed' => false, 'default' => 0, 'comment' => '会员充值订单操作日志ID'])
-            ->addColumn('operator_name', 'string', ['limit' => 50, 'default' => '', 'comment' => '操作员姓名'])
-            ->addColumn('operator_email', 'string', ['limit' => 50, 'default' => '', 'comment' => '操作员电子邮件'])
-            ->addColumn('client', 'string', ['limit' => 50, 'default' => '', 'comment' => '客户端信息'])
-            ->addColumn('message', 'string', ['limit' => 255, 'default' => '', 'comment' => '消息内容'])
-            ->addColumn('action', 'string', ['limit' => 255, 'default' => '', 'comment' => '操作'])
-            ->addColumn('data', 'string', ['limit' => 255, 'default' => '', 'comment' => '数据'])
-            ->addColumn('recharge_order_uuid', 'biginteger', ['signed' => false, 'default' => 0, 'comment' => '充值订单ID'])
-            ->addColumn('create_time', 'integer', ['signed' => false, 'default' => 0, 'comment' => '创建时间(时间戳)'])
-            ->addColumn('update_time', 'integer', ['signed' => false, 'default' => 0, 'comment' => '更新时间(时间戳)'])
-            ->addColumn('delete_time', 'integer', ['signed' => false, 'default' => 0, 'comment' => '删除时间(时间戳)'])
-            ->addIndex(['uuid'], ['unique' => true, 'name' => 'unique_uuid'])
-            ->create();
+        if (!$this->hasTable('return_order_amount')) {
+            $table->addColumn('id', 'integer', ['signed' => false, 'identity' => true, 'comment' => '自增ID'])
+                ->addColumn('uuid', 'biginteger', ['signed' => false, 'default' => 0, 'comment' => '会员充值订单操作日志ID'])
+                ->addColumn('operator_name', 'string', ['limit' => 50, 'default' => '', 'comment' => '操作员姓名'])
+                ->addColumn('operator_email', 'string', ['limit' => 50, 'default' => '', 'comment' => '操作员电子邮件'])
+                ->addColumn('client', 'string', ['limit' => 50, 'default' => '', 'comment' => '客户端信息'])
+                ->addColumn('message', 'string', ['limit' => 255, 'default' => '', 'comment' => '消息内容'])
+                ->addColumn('action', 'string', ['limit' => 255, 'default' => '', 'comment' => '操作'])
+                ->addColumn('data', 'string', ['limit' => 255, 'default' => '', 'comment' => '数据'])
+                ->addColumn('recharge_order_uuid', 'biginteger', ['signed' => false, 'default' => 0, 'comment' => '充值订单ID'])
+                ->addColumn('create_time', 'integer', ['signed' => false, 'default' => 0, 'comment' => '创建时间(时间戳)'])
+                ->addColumn('update_time', 'integer', ['signed' => false, 'default' => 0, 'comment' => '更新时间(时间戳)'])
+                ->addColumn('delete_time', 'integer', ['signed' => false, 'default' => 0, 'comment' => '删除时间(时间戳)'])
+                ->addIndex(['uuid'], ['unique' => true, 'name' => 'unique_uuid'])
+                ->create();
+        }
     }
 }
