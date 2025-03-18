099bb3ec68dc3060f6a1a379750232500b477852
xiezhihuan <602666178@qq.com>
2025-03-18 15:22:24 +0800

    feat: 完成扫码h5开桌相关接口

--- main/app/api/v1/h5/h5_handler.go
+++ main/app/api/v1/h5/h5_handler.go
@@ -3,6 +3,8 @@ package h5
 import (
 	"ttpos-server-go/app/api/helper"
 	"ttpos-server-go/app/constant"
+	"ttpos-server-go/app/dto"
+	"ttpos-server-go/app/dto/req"
 	"ttpos-server-go/app/errors"
 	"ttpos-server-go/app/service"
 	"ttpos-server-go/app/service/setting"
@@ -17,9 +19,12 @@ import (
 
 // H5Handler 商家H5端处理程序
 type H5Handler struct {
-	service   service.IH5Srv // h5扫码服务
-	deskSrv   service.IDeskSrv
-	buffetSrv service.IBuffetSrv
+	service        service.IH5Srv      // h5扫码服务
+	deskSrv        service.IDeskSrv    // 桌台服务
+	buffetSrv      service.IBuffetSrv  // 自助餐服务
+	productService service.IProductSrv // 产品服务
+	callSrv        service.ICallSrv    // 呼叫服务
+	orderService   service.IOrderSrv   // 订单服务
 }
 
 // BaseInfo 获取桌码基础信息
@@ -51,46 +56,53 @@ func (h H5Handler) BaseInfo(c *gin.Context) {
 // @Accept json
 // @Produce json
 // @Security JwtToken
-// @Success 200 {object} resp.H5Response{data=resp.H5BuffetList}
-// @Router /h5/index.php/scan/order.Order/buffetList [post]
+// @Success 200 {object} resp.BuffetListPaginationResp "自助餐列表"
+// @Router /h5/buffet/list [get]
 func (h *H5Handler) GetBuffetList(c *gin.Context) {
-	// ctx := helper.GetContext(c)
-	// var deskUuid uint64
-	// list, err := h.service.GetBuffetList(ctx, deskUuid)
-	// if err != nil {
-	// 	helper.H5Fail(c, 500, "获取信息失败")
-	// 	return
-	// }
-	// helper.H5Success(c, list)
+	companyUuid := helper.GetCompanyUuid(c)
+	// 获取自助餐列表
+	res, err := h.buffetSrv.GetBuffetList(companyUuid)
+	// 处理错误
+	if err != nil {
+		helper.ErrorWithDetail(c, constant.CodeFail, errors.WithMessage(err))
+		return
+	}
+	// 返回结果
+	helper.Success(c, res)
 }
 
-// GetOpenDesk 开台
+// CreateDeskOrder 处理创建开台
 // @Summary 开台
 // @Description 开台
 // @Tags 扫码点餐
 // @Accept json
 // @Produce json
 // @Security JwtToken
-// @param data body req.OpenDeskRequest true "创建扫码桌台订单参数"
-// @Success 200 {object} resp.H5Response{data=resp.H5BuffetList}
-// @Router /h5/index.php/scan/order.Order/setTable [post]
-func (h *H5Handler) GetOpenDesk(c *gin.Context) {
-	// ctx := helper.GetContext(c)
+// @param data body req.DeskOrderCreateReq true "开台参数"
+// @Success 200 {object} resp.CreateDeskOrderResp "开台成功"
+// @Failure 404 {object} nil "未找到"
+// @Router /h5/desk/open [post]
+func (h *H5Handler) OpenDesk(c *gin.Context) {
+	ctx := helper.GetContext(c)
+	// 绑定请求参数
+	params := req.DeskOrderCreateReq{}
+	if err := c.ShouldBind(&params); err != nil {
+		helper.HandleValidationError(c, err, params, nil)
+		return
+	}
+	// 获取桌台uuid
+	params.DeskUuid = ctx.GetDeskUuid()
+	// 创建桌台订单
+	res, err := h.deskSrv.CreateDeskOrder(ctx, params)
+	// 处理错误
+	if err != nil {
+		ctx.Log().Error("创建桌台订单失败", zap.Error(err))
+		helper.ErrorWithDetail(c, constant.CodeFail, errors.WithMessage(err))
+		return
+	}
 
-	// var deskUuid uint64
-	// // 绑定请求参数
-	// params := req.OpenDeskRequest{}
-	// if err := c.ShouldBind(&params); err != nil {
-	// 	helper.H5Fail(c, 500, "参数错误")
-	// 	return
-	// }
-
-	// err := h.service.OpenH5Desk(ctx, deskUuid, req.OpenDeskRequest{})
-	// if err != nil {
-	// 	helper.H5Fail(c, 500, "获取信息失败")
-	// 	return
-	// }
-	// helper.H5SuccessWithMsg(c, "开台成功")
+	// 返回结果
+	helper.Success(c, res)
 }
 
 // RemarkProduct 给商品添加备注
@@ -118,23 +130,114 @@ func (h *H5Handler) RemarkProduct(c *gin.Context) {
 	// helper.H5SuccessWithMsg(c, constant.RemarkSuccess)
 }
 
-// GetOpenDesk 获取商品分类
-// @Summary 获取商品分类
-// @Description 获取商品分类
+// GetProductCategoryList 获取收银产品类别列表
+// @Summary 获取收银产品类别列表
+// @Description 获取收银产品类别列表
 // @Tags 扫码点餐
 // @Accept json
 // @Produce json
 // @Security JwtToken
-// @Success 200 {object} resp.H5Response{data=resp.H5CategoryList}
-// @Router /h5/index.php/scan/product.category/index [post]
-func (h *H5Handler) GetCategoryList(c *gin.Context) {
-	// ctx := helper.GetContext(c)
-	// list, err := h.service.GetCategoryList(ctx)
-	// if err != nil {
-	// 	helper.H5Fail(c, 500, "获取信息失败")
-	// 	return
-	// }
-	// helper.H5Success(c, list)
+// @Success 200 {object} product_resp.ProductCategoryListResp "成功"
+// @Failure 400 {object} nil "错误请求"
+// @Router /h5/product/category/list [get]
+func (h *H5Handler) GetProductCategoryList(c *gin.Context) {
+	// 获取收银产品类别列表
+	res, err := h.productService.GetProductCategoryList(helper.GetCompanyUuid(c))
+
+	// 处理错误
+	if err != nil {
+		helper.ErrorWithDetail(c, constant.CodeFail, errors.WithMessage(err))
+		return
+	}
+
+	// 返回结果
+	helper.Success(c, res)
+}
+
+// GetProductList 获取收银产品列表
+// @Summary 获取收银产品列表
+// @Description 获取收银产品列表
+// @Tags 扫码点餐
+// @Accept json
+// @Produce json
+// @Security JwtToken
+// @Param page_no query int true "页码"
+// @Param page_size query int true "每页条数"
+// @Success 200 {object} product_resp.ProductListWithPaginationResp "成功"
+// @Failure 400 {object} nil "错误请求"
+// @Router /h5/product/list [get]
+func (h *H5Handler) GetProductList(c *gin.Context) {
+	// 绑定请求参数
+	req := req.ProductListReq{}
+	if err := c.ShouldBindQuery(&req); err != nil {
+		helper.HandleValidationError(c, err, req, dto.PageReqMessage)
+		return
+	}
+
+	// 获取收银产品列表
+	res, err := h.productService.GetProductList(helper.GetContext(c), req)
+
+	// 处理错误
+	if err != nil {
+		helper.ErrorWithDetail(c, constant.CodeFail, errors.WithMessage(err))
+		return
+	}
+
+	// 返回结果
+	helper.Success(c, res)
+}
+
+// Call 发起呼叫
+// @Summary 发起呼叫
+// @Description 发起呼叫
+// @Tags 扫码点餐
+// @Accept json
+// @Produce json
+// @Security JwtToken
+// @Param data body req.CallReq true "呼叫请求"
+// @Success 200 {object} dto.Response
+// @Router /h5/call [post]
+func (h *H5Handler) Call(c *gin.Context) {
+	var callReq req.CallReq
+	if err := c.ShouldBindJSON(&callReq); err != nil {
+		helper.HandleValidationError(c, err, callReq, nil)
+		return
+	}
+	err := h.callSrv.Call(helper.GetContext(c), callReq)
+	if err != nil {
+		helper.ErrorWithDetail(c, constant.CodeFail, errors.WithMessage(err))
+		return
+	}
+	helper.Success(c, gin.H{})
+}
+
+// OrderProductRemark 处理桌台订单商品备注
+// @Summary 桌台订单商品备注
+// @Description 桌台订单商品备注
+// @Tags 扫码点餐
+// @Accept json
+// @Produce json
+// @Security JwtToken
+// @param data body req.OrderProductRemarkReq true "详情参数"
+// @Success 200 {object} dto.Response{data=resp.ShopCart}
+// @Failure 404 {object} nil "未找到"
+// @Router /h5/remark [post]
+func (h *H5Handler) OrderProductRemark(c *gin.Context) {
+	ctx := helper.GetContext(c)
+	// 绑定请求参数
+	params := req.OrderProductRemarkReq{}
+	if err := c.ShouldBindJSON(&params); err != nil {
+		helper.HandleValidationError(c, err, params, nil)
+		return
+	}
+	//
+	info, err := h.orderService.OrderProductRemark(ctx, params)
+	if err != nil {
+		helper.ErrorWithDetail(c, constant.CodeFail, errors.WithMessage(err))
+		return
+	}
+	// 返回结果
+	helper.Success(c, info)
 }
 
 // RegisterH5Handlers 注册扫码h5路由
@@ -152,20 +255,27 @@ func RegisterH5Handlers(router gin.IRouter, dbm *database.DBManager, cache cache
 	deskSrv := service.NewDeskSrv(dbm, localeSrv, orderSrv, settingSrv, deviceSrv)
 	buffetSrv := service.NewBuffetSrv(dbm, localeSrv)
 	h5Srv := service.NewH5Srv(dbm, deskSrv, orderSrv, buffetSrv, settingSrv)
-
+	productService := service.NewProductSrv(dbm, localeSrv)
+	callSrv := service.NewCallSrv(dbm)
 	// 初始化处理器
 	wrapper := H5Handler{
-		service:   h5Srv,
-		deskSrv:   deskSrv,
-		buffetSrv: buffetSrv,
+		service:        h5Srv,
+		deskSrv:        deskSrv,
+		buffetSrv:      buffetSrv,
+		productService: productService,
+		callSrv:        callSrv,
+		orderService:   orderSrv,
 	}
 
 	// 需要认证
 	privateApi := router.Group("", middleware.DeskAuth(authSrv, dbm))
 	{
-		privateApi.GET("/base/info", wrapper.BaseInfo)
-		// privateApi.POST("/index.php/scan/base.base/getInfo", wrapper.BaseInfo)
-		privateApi.POST("/index.php/scan/order.Order/buffetList", wrapper.GetBuffetList)
-		privateApi.POST("/index.php/scan/order.Order/setTable", wrapper.GetOpenDesk)
+		privateApi.GET("/base/info", wrapper.BaseInfo)                           // 获取桌码基础信息
+		privateApi.GET("/buffet/list", wrapper.GetBuffetList)                    // 获取自助餐套餐列表
+		privateApi.POST("/desk/open", wrapper.OpenDesk)                          // 开台
+		privateApi.GET("/product/category/list", wrapper.GetProductCategoryList) // 获取收银产品类别列表
+		privateApi.GET("/product/list", wrapper.GetProductList)                  // 获取收银产品列表
+		privateApi.POST("/call", wrapper.Call)                                   // 发起呼叫
+		privateApi.POST("/remark", wrapper.RemarkProduct)                        // 给商品添加备注
 	}
 }
