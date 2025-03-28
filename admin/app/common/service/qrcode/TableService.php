46bdb5fbc0b8f2d38b8636edb733887ff4836a98
王昱 <nightcp874@gmail.com>
2025-03-28 18:43:33 +0800

    fix: 扫码无法打开H5和电子菜单

--- admin/app/common/service/qrcode/TableService.php
+++ admin/app/common/service/qrcode/TableService.php
@@ -44,7 +44,7 @@ class TableService extends Base
         if ($this->source == 'wx') {
             $qrCode = new QrCode($this->base_url . "pages/product/share-login/#/?token={$token}");
         } else if ($this->source == 'mp' || $this->source == 'h5') {
-            $qrCode = new QrCode($this->base_url . "scan/#/?token={$token}");
+            $qrCode = new QrCode($this->base_url . "/#/home?token={$token}");
         }
         $qrCode->setSize(300);
         $qrCode->setMargin(10);
