f95bc1f19cb8bcf64b54232514b1a13e040931d6
wfs <605403358@qq.com>
2025-04-07 16:52:43 +0800

    fix: 修复打印预结账单 应收金额

--- main/app/model/sale_order_ext_getset.go
+++ main/app/model/sale_order_ext_getset.go
@@ -109,6 +109,7 @@ func (model *SaleOrder) GetPrintReceivablePrice() float64 {
 	// 未结账时，需要计算最终应收金额
 	if model.Status != constant.SaleOrderStatusFinish {
 		finalPrice, _ = model.calcFinallyAmount()
+		finalPrice = finalPrice - model.GetCheckOutZeroFee()
 	}
 	// 如果是免单，返回0
 	if model.IsFreeSaleOrder() {
