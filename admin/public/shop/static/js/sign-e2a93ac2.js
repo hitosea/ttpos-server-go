c0f4ba51fd07fb253c994aea076c94fc02a6630c
hexiang <869411657@qq.com>
2025-04-09 18:58:45 +0800

    build shop

---
+++ admin/public/shop/static/js/sign-e2a93ac2.js
@@ -0,0 +1,1 @@
+import{r as s}from"./index-abf7a1b6.js";let t={getData:(t,i)=>s._get("/shop/plus.sign/index",t,i),saveData:(t,i)=>s._post("/shop/plus.sign/index",t,i),getSignList:(t,i)=>s._post("/shop/plus.sign/lists",t,i)};export{t as S};
