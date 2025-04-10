cc83bc049fec4fc0114d41e36326ea24493180c3
wfs <605403358@qq.com>
2025-04-10 16:32:14 +0800

    fix: 2.0.10-免单结账完成后，未打印结账单以及送厨单

--- main/app/event/free_sale_order_event_handler.go
+++ main/app/event/free_sale_order_event_handler.go
@@ -5,6 +5,8 @@ import (
 	"sync"
 	"ttpos-server-go/app/constant"
 	"ttpos-server-go/app/model"
+	"ttpos-server-go/app/printer"
+	"ttpos-server-go/app/printer/printer_model"
 	"ttpos-server-go/app/repository"
 	"ttpos-server-go/config"
 	"ttpos-server-go/pkg/database"
@@ -26,6 +28,54 @@ func init() {
 // freeSaleOrderEventHandler "免单"事件处理器
 func freeSaleOrderEventHandler() {
 	once_free_sale_order_event_handler.Do(func() {
+
+		// 创建菜单-付款打印
+		event.NewSystemBus().SubscribeFreeSaleOrderEvent(func(payload event.FreeSaleOrderPayload) {
+			saleOrder := payload.SaleBill.GetSaleOrder(payload.SaleOrderUuid)
+			if saleOrder == nil {
+				return
+			}
+			products := printer_model.Products{}
+			for _, saleOrderProduct := range saleOrder.SaleOrderProducts {
+				if saleOrderProduct.IsCancelProduct() || !saleOrderProduct.IsSendKitchen() {
+					continue
+				}
+				products = append(products, printer_model.OrderProduct{
+					OrderProductId:  saleOrderProduct.Uuid,
+					ProductId:       saleOrderProduct.ProductPackageUuid,
+					ProductName:     saleOrderProduct.MultiLanguageName.GetNames(),
+					ProductAttr:     saleOrderProduct.GetAttributeName(),
+					ProductAttrList: saleOrderProduct.GetAttributeNameList(),
+					TotalNum:        saleOrderProduct.Num,
+					IsBuffet:        saleOrderProduct.IsBuffet == 1,
+					Remark:          saleOrderProduct.Remark,
+				})
+			}
+
+			if len(products) > 0 {
+				printer.NewPrinterRepo(payload.Ctx, "").PrintingDishes(
+					constant.PrinterProductTypePay,
+					payload.SaleBillUuid,
+					products,
+				)
+			}
+		})
+
+		// 创建结账单打印
+		event.NewSystemBus().SubscribeFreeSaleOrderEvent(func(payload event.FreeSaleOrderPayload) {
+			_, err := printer.NewPrinterRepo(payload.Ctx).PrintingStatementOrder(
+				constant.PrinterTemplateBilling,
+				payload.SaleBill,
+				payload.SaleOrderUuid,
+				0,
+				0,
+			)
+			if err != nil {
+				fmt.Println("FreeSaleOrderEvent process, PrintingStatementOrder failed ", err)
+			}
+		})
+
+		// 创建操作记录
 		event.NewSystemBus().SubscribeFreeSaleOrderEvent(func(payload event.FreeSaleOrderPayload) {
 			db := database.GetDBManager(config.DatabaseConf{}).GetDB(payload.CompanyUuid)
 			orderRecordRepo := repository.NewOrderOperationRecordRepo(db)
