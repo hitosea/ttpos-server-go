ca1988c251f714367127e4955b61044f23a0e7cb
曾振华 <zhenhua.zeng@qq.com>
2025-04-18 17:39:17 +0800

    fix: 未找到需要拒单的h5订单则跳过

--- main/app/service/order.go
+++ main/app/service/order.go
@@ -8859,7 +8859,8 @@ func (s *orderSrv) RejectH5Order(ctx context.Context, h5OrderUuid uint64) error 
 	h5OrderRepo := repository.NewH5OrderRepo(db)
 	// 获取h5订单
 	h5Order, err := h5OrderRepo.GetH5OrderDetail(h5OrderUuid)
-	if err != nil {
+	
+	if err != nil && !builtinerrors.Is(err, gorm.ErrRecordNotFound) {
 		return errors.WithMessage(errors.ErrInternal, "获取h5订单失败", err.Error())
 	}
 	// 非待处理状态不可操作
