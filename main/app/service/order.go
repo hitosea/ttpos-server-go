c9e357d372c14c24b19971429563de9cbcb9dc8b
wfs <605403358@qq.com>
2025-04-18 16:26:50 +0800

    fix:  锁单 状态未及时同步 - 收银机 锁单/解锁 ->  点餐助手 的桌台列表、桌台详情未同步更新状态

--- main/app/service/order.go
+++ main/app/service/order.go
@@ -8416,7 +8416,7 @@ func (s *orderSrv) OrderPrint(ctx context.Context, request req.OrderPrintReq, ne
 		// 推送桌台更新
 		go websocket.PushClient(ctx.GetCompanyUuid(), websocket.SourceAll, websocket.SourceAll, websocket.UPDATE_DESK, map[string]interface{}{
 			"desk_uuid":   saleBill.DeskUuid,
-			"update_time": saleBill.BaseModel.UpdateTime,
+			"update_time": time.Now().Unix(),
 		})
 	}
 
