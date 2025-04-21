cd250c6db3de3da2870c7ece0cc62af590735089
王昱 <nightcp874@gmail.com>
2025-04-21 10:38:29 +0800

    fix: 2.0.10-客户端-商品显示问题汇总

--- main/app/service/product.go
+++ main/app/service/product.go
@@ -53,7 +53,6 @@ func (s *productSrv) GetProductList(ctx context.Context, req req.ProductListReq)
 	if option, ok := sourceMap[ctx.GetSource()]; ok {
 		dbOptions = append(dbOptions, option)
 	}
-	dbOptions = append(dbOptions, commonRepo.WhereByStatus(1), commonRepo.WhereBySoftDelete(), commonRepo.SortWithSort("ASC"), commonRepo.SortWithID("DESC"))
 	products, total, err := productRepo.GetProductListWithPagination(
 		req.PageNo,
 		req.PageSize,
