da94c5b4b83091634161aeed966384a2a9dedbf3
wfs <605403358@qq.com>
2025-04-23 11:03:32 +0800

    fix: 修复免单未计入高峰时段

--- main/app/service/order.go
+++ main/app/service/order.go
@@ -7181,6 +7181,7 @@ func (s *orderSrv) InstantOrderFree(ctx context.Context, req req.InstantOrderFre
 			DiscountMoney: saleOrder.GetAmount(),
 		})
 	}()
+
 	// 发布"统计"事件
 	go func() {
 		s.bus.PublishStatisticsSaleEvent(event.StatisticsSalePayload{
