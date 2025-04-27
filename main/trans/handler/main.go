020f3384784eaf65347d3a1c1d0f8968546f349b
wfs <605403358@qq.com>
2025-04-27 10:51:06 +0800

    feat:  完善迁移指令

--- main/trans/handler/main.go
+++ main/trans/handler/main.go
@@ -6,7 +6,7 @@ import (
 	"gorm.io/gorm"
 )
 
-func Run(sourceDB *gorm.DB, targetDB *gorm.DB) error {
+func Run(sourceDB *gorm.DB, targetDB *gorm.DB, targetSassDB *gorm.DB) error {
 	userService := v1.NewUserGradeService(sourceDB, targetDB)
 	err := userService.ConvertUserGrade()
 	if err != nil {
