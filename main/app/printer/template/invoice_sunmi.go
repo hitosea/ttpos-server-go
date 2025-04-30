9926c8e48a89cc114076683445742536fbd738b0
wfs <605403358@qq.com>
2025-04-30 18:50:49 +0800

    fix: 修复打印

--- main/app/printer/template/invoice_sunmi.go
+++ main/app/printer/template/invoice_sunmi.go
@@ -364,7 +364,7 @@ func (t *invoiceSunmiTemplate) GetPrintContent(
 	// Print and exit page mode
 	printer.LineFeed(6)
 	printer.PrintAndExitPageMode()
-	printer.CutPaper(true)
+	printer.CutPaper(!isOneself)
 
 	// 返回打印数据
 	return printer.GetOrderData()
