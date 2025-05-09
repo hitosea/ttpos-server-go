31fdc2f7e97b964f001b1f50935d5a9c7e2c6095
王昱 <nightcp874@gmail.com>
2025-05-10 05:20:08 +0800

    fix: 服务费为1%时，服务费计算错误

--- main/app/service/order.go
+++ main/app/service/order.go
@@ -324,7 +324,7 @@ func parseServiceFeeRate(ServiceChargeRate string) (float64, error) {
 		return 0, errors.WithMessage(err)
 	}
 	// 兼容取值范围0-100的情况。本系统中比例的统一取值范围是0-1，所以需要转换
-	if serviceFeeValue > 1 {
+	if serviceFeeValue > 0 {
 		// 将取值范围0-100转换为0-1
 		serviceFeeValue = decimal.NewFromFloat(serviceFeeValue).Div(decimal.NewFromInt(100)).Truncate(2).InexactFloat64()
 		// 取值范围0-1
