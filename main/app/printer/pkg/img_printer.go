fa0a2a2c53c0deb8f42d78900b0392ef129929a5
wfs <605403358@qq.com>
2025-05-20 16:56:41 +0800

    feat:  修复图片打印 - 日语

--- main/app/printer/pkg/img_printer.go
+++ main/app/printer/pkg/img_printer.go
@@ -146,7 +146,7 @@ func (i *ImgFont) createImg() {
 func (i *ImgFont) GetFontPath(char string) string {
 	// 中文
 	chiPattern := regexp.MustCompile(`[\x{4E00}-\x{9FFF}\x{FF01}\x{FF0C}\x{FF08}\x{FF09}\x{FF1A}\x{FF5E}\x{2014}]`)
-	if chiPattern.MatchString(char) || char == "。" || char == "、" || char == "-" || char == "￥" || char == "；" || char == "？" || char == "＋" || char == "：" {
+	if char != "ー" && (chiPattern.MatchString(char) || char == "。" || char == "、" || char == "-" || char == "￥" || char == "；" || char == "？" || char == "＋" || char == "：") {
 		return fonts.FontZH
 	}
 
@@ -164,7 +164,7 @@ func (i *ImgFont) GetFontPath(char string) string {
 
 	// 日文
 	japPattern := regexp.MustCompile(`[\p{Hiragana}\p{Katakana}\p{Han}★]+`)
-	if japPattern.MatchString(char) {
+	if char == "ー" || japPattern.MatchString(char) {
 		return fonts.FontJA
 	}
 
