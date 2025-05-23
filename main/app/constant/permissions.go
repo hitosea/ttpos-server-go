0e070246939e0dc53a75395ce7c367e49a8efd48
wfs <605403358@qq.com>
2025-05-23 09:29:33 +0800

    feat:  点餐助手添加权限

--- main/app/constant/permissions.go
+++ main/app/constant/permissions.go
@@ -5,3 +5,9 @@ var CashierPermissions = map[string]string{
 	"/api/v1/cashier/desk/open":  "cashier_table_open",
 	"/api/v1/cashier/desk/close": "cashier_table_delete",
 }
+
+// 点餐助手权限
+var AssistantPermissions = map[string]string{
+	"/api/v1/assistant/desk/close":        "clear_table",
+	"/api/v1/assistant/desk/order/cancel": "clear_table",
+}
