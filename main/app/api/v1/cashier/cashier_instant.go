58f500dbf3310d58bf78bb3ef5be8bb71ed48eed
xiezhihuan <602666178@qq.com>
2025-06-06 14:09:25 +0800

    feat: 收银点餐、助手端增加手动积分设置接口

--- main/app/api/v1/cashier/cashier_instant.go
+++ main/app/api/v1/cashier/cashier_instant.go
@@ -693,6 +693,37 @@ func (h *InstantHandler) OrderPaymentQrcodeInfo(c *gin.Context) {
 	helper.Success(c, res)
 }
 
+// OrderPaymentInfo 设置订单的抵扣积分数量
+// @Summary 设置订单的抵扣积分数量
+// @Description 设置订单的抵扣积分数量
+// @Tags 收银端.桌台.结账
+// @Accept json
+// @Produce json
+// @Security JwtToken
+// @param data body req.InstantOrderPaymentPointsReq true "设置订单的抵扣积分数量参数"
+// @Success 200 {object} dto.Response{data=resp.InstantOrderPaymentInfoResp} "结账页面信息"
+// @Failure 404 {object} nil "未找到"
+// @Router /cashier/instant/order/payment/points [post]
+func (h *InstantHandler) OrderPaymentPoints(c *gin.Context) {
+	ctx := helper.GetContext(c)
+	ctx.Log().Debug("收到桌台页面设置订单的抵扣积分数量接口请求")
+
+	params := req.InstantOrderPaymentPointsReq{}
+	if err := c.ShouldBindJSON(&params); err != nil {
+		helper.ErrorWithDetail(c, constant.CodeFail, errors.WithMessage(err))
+		return
+	}
+	ctx.Log().Info("设置订单的抵扣积分数量", zap.Any("params", params))
+	// 设置订单的抵扣积分数量
+	res, err := h.orderSrv.OrderPaymentPoints(ctx, params)
+	if err != nil {
+		helper.ErrorWithDetail(c, constant.CodeFail, errors.WithMessage(err))
+		return
+	}
+	// 返回结果
+	helper.Success(c, res)
+}
+
 // OrderPaymentCreate 创建一个支付单
 // @Summary 创建一个支付单
 // @Description 创建一个支付单
@@ -1272,6 +1303,7 @@ func RegisterInstantHandlers(router gin.IRouter, dbm *database.DBManager, cache 
 		privateApi.POST("/instant/order/must_plan/confirm", wrapper.OrderMustPlanConfirm)                        // 确认必点商品
 		privateApi.GET("/instant/order/check", wrapper.OrderCheck)                                               // 订单检查。场景：1、点击结账按钮时，检查订单是否可以结账
 		privateApi.GET("/instant/order/payment/info", wrapper.OrderPaymentInfo)                                  // 获取结账页面信息
+		privateApi.POST("/instant/order/payment/points", wrapper.OrderPaymentPoints)                             // 设置订单的抵扣积分数量
 		privateApi.POST("/instant/order/payment/create", wrapper.OrderPaymentCreate)                             // 创建一个支付单
 		privateApi.POST("/instant/order/payment/cancel", wrapper.OrderPaymentCancel)                             // 撤销一个支付单
 		privateApi.POST("/instant/order/payment/finish", wrapper.OrderPaymentFinish)                             // 完成销售订单的付款结账
