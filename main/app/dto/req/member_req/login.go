6628a52b89ce1c029a9651c76c77a6307949013a
wfs <605403358@qq.com>
2025-06-13 11:53:50 +0800

    feat:  优化提示

--- main/app/dto/req/member_req/login.go
+++ main/app/dto/req/member_req/login.go
@@ -19,6 +19,10 @@ func (req *MemberSendCodeReq) Validate() error {
 	if req.CompanyUuid == 0 {
 		return errors.New("商家ID不能为空")
 	}
+	// 增加全数字校验
+	if !isNumeric(req.Phone) {
+		return errors.New("手机号格式不正确")
+	}
 	// 判断 req.Phone 是否是手机号
 	if req.AreaCode == constant.ChinaPrefix {
 		if len(req.Phone) != 11 {
@@ -46,6 +50,16 @@ type MemberLoginReq struct {
 	Code        string `json:"code" form:"code"`                 // 验证码
 }
 
+// 判断是否全为数字
+func isNumeric(s string) bool {
+	for _, v := range s {
+		if v < '0' || v > '9' {
+			return false
+		}
+	}
+	return true
+}
+
 func (req *MemberLoginReq) Validate() error {
 	if req.CompanyUuid == 0 {
 		return errors.New("商家ID不能为空")
@@ -55,6 +69,16 @@ func (req *MemberLoginReq) Validate() error {
 	}
 	if req.Code == "" {
 		return errors.New("验证码不能为空")
+	}
+	// 增加全数字校验
+	if !isNumeric(req.Phone) || len(req.Phone) != 11 && len(req.Phone) != 10 {
+		return errors.New("手机号格式不正确")
+	}
+	// 判断 req.Phone 是否是手机号
+	if len(req.Phone) == 11 && req.Phone[0] != '1' {
+		return errors.New("手机号格式不正确")
+	} else if len(req.Phone) == 10 && req.Phone[0] != '0' {
+		return errors.New("手机号格式不正确")
 	}
 	return nil
 }
