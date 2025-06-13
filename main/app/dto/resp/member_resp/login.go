6239561c8a7bd8ad3f7ccd08a9c547e86faa677b
wfs <605403358@qq.com>
2025-06-13 11:00:32 +0800

    feat:  会员信息登录页返回对应的语言

--- main/app/dto/resp/member_resp/login.go
+++ main/app/dto/resp/member_resp/login.go
@@ -1,8 +1,11 @@
 package member_resp
 
+import "ttpos-server-go/app/dto"
+
 type MemberLoginInfoResp struct {
-	CompanyUuid uint64   `json:"company_uuid"` // 集团UUID
-	CompanyName string   `json:"company_name"` // 集团名称
-	Logo        string   `json:"logo"`         // 集团logo
-	AreaCode    []string `json:"area_code"`    // 地区码数组，如 ["+86", "+1", "+44"]
+	CompanyUuid  uint64             `json:"company_uuid"`  // 集团UUID
+	CompanyName  string             `json:"company_name"`  // 集团名称
+	Logo         string             `json:"logo"`          // 集团logo
+	AreaCode     []string           `json:"area_code"`     // 地区码数组，如 ["+86", "+1", "+44"]
+	LanguageList []dto.LanguageItem `json:"language_list"` // 语言列表
 }
