b9da03832e462ad460ef5b766e66b09a3d3602af
wfs <605403358@qq.com>
2025-06-16 18:13:42 +0800

    fix: 优化打印的间隔

--- main/app/model/printer_log.go
+++ main/app/model/printer_log.go
@@ -144,9 +144,9 @@ func (model *PrinterLog) CalculationTime() int64 {
 	if model.PrinterType == constant.PrinterTypeXPrinterWifi {
 		speed = 120
 	} else if model.PrinterType == constant.PrinterTypeCodesoftWifi {
-		speed = 80
+		speed = 90
 	} else if model.PrinterType == constant.PrinterTypeCodesoftLan {
-		speed = 100
+		speed = 120
 	}
 	//
 	t = int64(math.Ceil(float64(len(model.Data)) / float64(speed)))
