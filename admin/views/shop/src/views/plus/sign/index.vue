71e0ac51d733c0042c5fa56e0603ca41b3e668e7
hexiang <869411657@qq.com>
2025-06-24 10:51:05 +0800

    feat: 新增退菜单模版2，去掉JBC的logo

--- admin/views/shop/src/views/plus/sign/index.vue
+++ admin/views/shop/src/views/plus/sign/index.vue
@@ -6,22 +6,18 @@
   -->
   <div class="common-seach-wrap">
     <el-tabs v-model="activeName">
-      <el-tab-pane label="签到设置" name="setting">
-      </el-tab-pane>
-      <el-tab-pane label="签到记录" name="receive">
-      </el-tab-pane>
+      <el-tab-pane label="签到设置" name="setting"> </el-tab-pane>
+      <el-tab-pane label="签到记录" name="receive"> </el-tab-pane>
     </el-tabs>
 
     <!--签到设置-->
-    <Setting v-if="activeName=='setting'"></Setting>
+    <Setting v-if="activeName == 'setting'"></Setting>
 
     <!--签到记录-->
-    <Receive v-if="activeName=='receive'"></Receive>
-
+    <Receive v-if="activeName == 'receive'"></Receive>
   </div>
 </template>
 <script>
-
   import Setting from './setting/Setting.vue';
   import Receive from './receive/Receive.vue';
   export default {
@@ -32,10 +28,9 @@
     data() {
       return {
         /*当前选中*/
-        activeName: 'setting'
+        activeName: 'setting',
       };
     },
-    methods: {}
+    methods: {},
   };
 </script>
-
