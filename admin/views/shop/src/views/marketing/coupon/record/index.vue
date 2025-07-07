27dcd037171db3426192e94a498853bf95d781a3
hexiang <869411657@qq.com>
2025-07-07 09:12:11 +0800

    fix: 2.3.0-优惠券使用-使用营销活动优惠券后反结账，优惠券记录无该优惠券的反结账记录

--- admin/views/shop/src/views/marketing/coupon/record/index.vue
+++ admin/views/shop/src/views/marketing/coupon/record/index.vue
@@ -12,7 +12,7 @@
             <el-option :label="$t('首次添加')" value="1"></el-option>
             <el-option :label="$t('调整添加')" value="2"></el-option>
             <el-option :label="$t('调整扣减')" value="3"></el-option>
-            <el-option :label="$t('活动扣减')" value="4"></el-option>
+            <el-option :label="$t('反结账退还')" value="4"></el-option>
             <el-option :label="$t('奖励领取（冻结）')" value="5"></el-option>
             <el-option :label="$t('核销扣减')" value="6"></el-option>
           </a-select>
@@ -53,7 +53,7 @@
               <span v-if="scope.row.record_type == 1">{{ $t('首次添加') }}</span>
               <span v-if="scope.row.record_type == 2">{{ $t('调整添加') }}</span>
               <span v-if="scope.row.record_type == 3">{{ $t('调整扣减') }}</span>
-              <span v-if="scope.row.record_type == 4">{{ $t('活动扣减') }}</span>
+              <span v-if="scope.row.record_type == 4">{{ $t('反结账退还') }}</span>
               <span v-if="scope.row.record_type == 5">{{ $t('奖励领取（冻结）') }}</span>
               <span v-if="scope.row.record_type == 6">{{ $t('核销扣减') }}</span>
             </template>
