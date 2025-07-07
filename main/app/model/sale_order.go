46101f9c4d4520cb6a2d4d8cf50b4c9797e65472
xiezhihuan <602666178@qq.com>
2025-07-07 17:50:25 +0800

    fix: 下单后固定商品的num_type

--- main/app/model/sale_order.go
+++ main/app/model/sale_order.go
@@ -488,7 +488,7 @@ func (model *SaleOrder) GetProductList(hasOrderedH5ProductWithReject bool) []res
 			LocaleName:          saleOrderProduct.MultiLanguageName.GetNames(),
 			LocaleAttributeName: saleOrderProduct.GetAttributeName(),
 			Num:                 saleOrderProduct.Num,
-			NumType:             saleOrderProduct.ProductPackage.NumType,
+			NumType:             saleOrderProduct.NumType,
 			SalePrice:           saleOrderProduct.GetSalePrice(),
 			DiscountPrice:       saleOrderProduct.GetProductFinalSalePrice(),
 			Status:              saleOrderProduct.StatusValue(),
