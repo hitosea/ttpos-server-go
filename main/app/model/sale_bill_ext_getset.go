4c57c44372e40593cafc007c6b698ce23ab447af
xiezhihuan <602666178@qq.com>
2025-07-08 11:46:39 +0800

    fix: 反结账时将sale_bill和sale_order的finish_time置0

--- main/app/model/sale_bill_ext_getset.go
+++ main/app/model/sale_bill_ext_getset.go
@@ -596,8 +596,9 @@ func (model *SaleBill) SetReverseSettle() {
 	// 销售订单状态变为未结账状态
 	// 销售订单的所有付款单都退款，并生成退款单
 	model.Status = constant.SaleBillStatusPending
-	model.FinishTime = 0
+	model.FinishTime = 0 // 反结账后支付时间finish_time置0
 	for _, saleOrder := range model.SaleOrders {
+		saleOrder.FinishTime = 0 // 反结账后支付时间finish_time置0
 		saleOrder.Status = constant.SaleOrderStatusPending
 		for _, paymentOrder := range saleOrder.PaymentOrders {
 			if !paymentOrder.PaymentMethod.IsLianLianPay() {
