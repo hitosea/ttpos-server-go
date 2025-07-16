f7b301fb06f6e2a9c8b150340108a95273b69537
xiezhihuan <602666178@qq.com>
2025-07-16 17:40:14 +0800

    fix: 更新API接口文档

--- main/app/api/v1/cashier/cashier_member_order.go
+++ main/app/api/v1/cashier/cashier_member_order.go
@@ -19,16 +19,16 @@ type MemberOrderHandler struct {
 	memberOrderSrv service.IMemberOrderSrv
 }
 
-// GetMemberOrderList 会员端订单列表
-// @Summary 会员端订单列表
-// @Description 会员端订单列表
-// @Tags 收银端.外送单相关
+// GetMemberOrderList 外送订单接单列表
+// @Summary 外送订单接单列表
+// @Description 外送订单接单列表
+// @Tags 收银端.外送接单相关
 // @Accept json
 // @Produce json
 // @Security JwtToken
 // @Param page_no query int false "页码"
 // @Param page_size query int false "每页条数"
-// @Param status query string true "状态:"unaccept" 待接单, "accept" 备餐中, "undelivery" 待配送, "delivery" 配送中, "completed" 已完成, "cancel" 已取消"
+// @Param status query string true "状态: unaccept-待接单, accept-备餐中, undelivery-待配送, delivery-配送中, completed-已完成, cancel-已取消"
 // @Success 200 {object} dto.Response{data=resp.GetMemberOrderListResp}
 // @Router /cashier/member_order/list [get]
 func (h *MemberOrderHandler) GetMemberOrderList(c *gin.Context) {
@@ -45,10 +45,10 @@ func (h *MemberOrderHandler) GetMemberOrderList(c *gin.Context) {
 	helper.Success(c, res)
 }
 
-// GetMemberOrderDetail 会员端订单详情
-// @Summary 会员端订单详情
-// @Description 会员端订单详情
-// @Tags 收银端.外送单相关
+// GetMemberOrderDetail 外送订单详情
+// @Summary 外送订单详情
+// @Description 外送订单详情
+// @Tags 收银端.外送接单相关
 // @Accept json
 // @Produce json
 // @Security JwtToken
@@ -91,7 +91,7 @@ func RegisterMemberOrderHandlers(router gin.IRouter, dbm *database.DBManager, ca
 	// 需要认证
 	privateApi := router.Group("", middleware.Auth(authSrv, dbm))
 	{
-		privateApi.GET("/member_order/list", wrapper.GetMemberOrderList)     // 获取会员端订单列表
-		privateApi.GET("/member_order/detail", wrapper.GetMemberOrderDetail) // 获取会员端订单详情
+		privateApi.GET("/member_order/list", wrapper.GetMemberOrderList)     // 获取外送订单接单列表
+		privateApi.GET("/member_order/detail", wrapper.GetMemberOrderDetail) // 获取外送订单接单详情
 	}
 }
