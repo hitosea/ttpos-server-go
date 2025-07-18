8b357a69d1b29930b3e722d31e82fe787663b62b
wfs <605403358@qq.com>
2025-07-18 12:52:24 +0800

    feat:  优化打印间隔

--- main/app/printer/template/statement_order_img.go
+++ main/app/printer/template/statement_order_img.go
@@ -349,7 +349,7 @@ func (t *statementOrderImgTemplate) GetPrintContent(
 	for key, product := range products {
 		img.SetTextLineHeight(45)
 		img.PrintInColumns(
-			pkg.ColumnConfig{Text: product.ProductName, Width: 310, Align: pkg.AlignLeft},
+			pkg.ColumnConfig{Text: product.ProductName, Width: 300, ContentWidth: utils.IfInt(t.base.Lang == "th", 270, 300), Align: pkg.AlignLeft},
 			pkg.ColumnConfig{Text: fmt.Sprintf("%s*%v", t.base.Amount(product.ProductPrice), product.ProductNum), Width: 120, Align: pkg.AlignCenter},
 			pkg.ColumnConfig{Text: t.base.GetPriceAndUnit(product.ProductTotalPrice), Width: 0, Align: pkg.AlignRight},
 		)
