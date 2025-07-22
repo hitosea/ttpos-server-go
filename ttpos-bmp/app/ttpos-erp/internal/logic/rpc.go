783ccd37df946d7ccca16b0c47bcf214f2e4327c
rikugun <rikugun@ymail.com>
2025-07-22 10:15:14 +0800

    feat: 增加 erpnext 对接api

---
+++ ttpos-bmp/app/ttpos-erp/internal/logic/rpc.go
@@ -0,0 +1,33 @@
+package logic
+
+import (
+	"context"
+	"github.com/gogf/gf/contrib/rpc/grpcx/v2"
+	"github.com/gogf/gf/v2/frame/g"
+	"google.golang.org/grpc"
+	"ttpos-bmp/internal/pkg/nacos/service"
+)
+
+var (
+	managerConn *grpc.ClientConn
+)
+
+func InitRpcClient(ctx context.Context) {
+	managerConn = grpcx.Client.MustNewGrpcClientConn(g.Cfg().MustGet(ctx, "grpc.name").String())
+}
+
+func initRpcServer() {
+	go service.RpcServer.GRpc.Run()
+}
+
+func InitRpc(ctx context.Context) {
+	//加载注册nacos grpc服务
+	service.RpcServer.Init(ctx)
+	//加载nacos grpc客户端
+	service.RpcClient.Init(ctx)
+
+	initRpcServer()
+
+	InitRpcClient(ctx)
+
+}
