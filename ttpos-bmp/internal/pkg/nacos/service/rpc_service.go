312558c7eb0e05e2fd1c1a36f38a51d7761f0543
rikugun <rikugun@ymail.com>
2025-07-29 17:59:42 +0800

    feat: 增加grpc 暴露endpoint 从环境变量中获取功能

--- ttpos-bmp/internal/pkg/nacos/service/rpc_service.go
+++ ttpos-bmp/internal/pkg/nacos/service/rpc_service.go
@@ -7,6 +7,8 @@ import (
 	"github.com/gogf/gf/contrib/rpc/grpcx/v2"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/gsvc"
+	"github.com/gogf/gf/v2/os/genv"
+	"github.com/gogf/gf/v2/text/gstr"
 )
 
 var RpcServer = new(rpcServer)
@@ -16,11 +18,17 @@ type rpcServer struct {
 }
 
 func (s *rpcServer) Init(ctx context.Context) {
+	grpcConf := grpcx.Server.NewConfig()
+	//特殊处理 grpc.endpoints ,支持从环境变量中获取
+	if endpoints := genv.Get("GRPC_ENDPOINTS", "").String(); len(endpoints) > 0 {
+		g.Log().Infof(ctx, "使用环境变量注册服务 GRPC_ENDPOINTS: %v", endpoints)
+		grpcConf.Endpoints = gstr.Split(endpoints, ",")
+	}
 	grpcx.Resolver.Register(nacos.New(GetNacosAddress(ctx)))
 	//使用默认组和集群名称
 	//.SetClusterName("DEFAULT")
 	//.SetGroupName("DEFAULT_GROUP"))
-	s.GRpc = grpcx.Server.New()
+	s.GRpc = grpcx.Server.New(grpcConf)
 	//注册服务
 	//go s.GRpc.Run()
 }
