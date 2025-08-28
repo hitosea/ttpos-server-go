1566091f1bcf144424b9320977c2dfb9ae69e3ff
王昱 <nightcp874@gmail.com>
2025-08-28 09:37:21 +0800

    fix: 修复商品套餐子产品的删除时间判断逻辑

--- main/app/model/product.go
+++ main/app/model/product.go
@@ -321,12 +321,12 @@ func (model *ProductPackage) GetRespAttributeGroupList() []product_resp.ProductA
 func (model *ProductPackage) GetRespPackageSubProductGroupList() []product_resp.ProductPackageSubProductGroup {
 	packageSubProductGroupList := make([]product_resp.ProductPackageSubProductGroup, 0)
 	for _, packageSubProductGroup := range model.ProductPackageGroups {
-		if packageSubProductGroup.DeleteTime == 0 {
+		if packageSubProductGroup.DeleteTime > 0 {
 			continue
 		}
 		products := make([]product_resp.ProductPackageSubProduct, 0)
 		for _, product := range packageSubProductGroup.ProductPackageGroupItems {
-			if product.DeleteTime == 0 {
+			if product.DeleteTime > 0 {
 				continue
 			}
 			products = append(products, product_resp.ProductPackageSubProduct{
