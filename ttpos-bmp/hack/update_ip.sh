e5c0fd0db1db2a2dd6688d1e0e8f6b93be1dd4ad
rikugun <v5.rikugun@gmail.com>
2025-09-01 13:42:28 +0800

    fix: 修复兼容问题

--- ttpos-bmp/hack/update_ip.sh
+++ ttpos-bmp/hack/update_ip.sh
@@ -6,14 +6,14 @@ ENV_FILE=../.env
 if [ -f "$ENV_FILE" ] ;then
   echo "替换中台.env 本地IP"
   #替换本地IP
-  sed -i.bak "s/^DB_HOST=.*/DB_HOST=${LOCAL_IP}/" .env && rm .env.bak;
-  sed -i.bak "s/^GRPC_ENDPOINTS=.*/GRPC_ENDPOINTS=${LOCAL_IP}/" .env && rm .env.bak;
-  sed -i.bak "s/^REDIS_HOST=.*/REDIS_HOST=${LOCAL_IP},${LOCAL_IP},${LOCAL_IP}/" .env && rm .env.bak;
-  sed -i.bak "s/^REDIS_PORT=.*/REDIS_PORT=7001,7002,7003/" .env && rm .env.bak;
-  if grep -q '^REDIS_CLUSTER_ANNOUNCE_IP=' .env; then \
-    sed -i.bak "s/^REDIS_CLUSTER_ANNOUNCE_IP=.*/REDIS_CLUSTER_ANNOUNCE_IP=${LOCAL_IP}/" .env && rm .env.bak; \
-  else \
-    echo "\nREDIS_CLUSTER_ANNOUNCE_IP=${LOCAL_IP}" >> .env; \
+  sed -i.bak "s/^DB_HOST=.*/DB_HOST=${LOCAL_IP}/" .env && rm .env.bak
+  sed -i.bak "s/^GRPC_ENDPOINTS=.*/GRPC_ENDPOINTS=${LOCAL_IP}/" .env && rm .env.bak
+  sed -i.bak "s/^REDIS_HOST=.*/REDIS_HOST=${LOCAL_IP},${LOCAL_IP},${LOCAL_IP}/" .env && rm .env.bak
+  sed -i.bak "s/^REDIS_PORT=.*/REDIS_PORT=7001,7002,7003/" .env && rm .env.bak
+  if grep -q '^REDIS_CLUSTER_ANNOUNCE_IP=' .env; then
+    sed -i.bak "s/^REDIS_CLUSTER_ANNOUNCE_IP=.*/REDIS_CLUSTER_ANNOUNCE_IP=${LOCAL_IP}/" .env && rm .env.bak
+  else
+    echo "REDIS_CLUSTER_ANNOUNCE_IP=${LOCAL_IP}" >> .env
   fi
    echo "替换中台.env 本地IP成功 $LOCAL_IP"
 fi
