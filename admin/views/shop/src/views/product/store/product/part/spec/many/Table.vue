4fc3320b030c49ba378fdebf8e89274faa482b03
hexiang <869411657@qq.com>
2025-09-01 14:41:41 +0800

    fix:  商品条形码13位数

--- admin/views/shop/src/views/product/store/product/part/spec/many/Table.vue
+++ admin/views/shop/src/views/product/store/product/part/spec/many/Table.vue
@@ -100,12 +100,6 @@
                 :rules="[
                   {
                     validator: (rule, value, callback) => {
-                      // 判断长度是否为12或13位，并且只能输入数字
-                      if (value && value.length !== 12 && value.length !== 13) {
-                        callback(new Error($t('长度12-13位数字')));
-                        return;
-                      }
-
                       if (!scope.row.barcodeUniqueness) {
                         callback(new Error($t('商品条码重复')));
                         return;
