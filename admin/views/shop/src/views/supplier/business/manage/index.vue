590130fbe9c84789ca9b8b22163928708299a691
hexiang <869411657@qq.com>
2025-09-01 16:26:26 +0800

    fix:  文案修改

--- admin/views/shop/src/views/supplier/business/manage/index.vue
+++ admin/views/shop/src/views/supplier/business/manage/index.vue
@@ -143,8 +143,8 @@
       <el-form-item for="no_click" :label="$t('营业时间')" prop="opening_hours" :rules="[{ required: true, message: $t('请选择营业时间') }]">
         <TimePicker v-model="form.opening_hours" @update:modelValue="updateOpeningHours" />
       </el-form-item>
-      <el-form-item for="no_click" :label="$t('起始序列号')" prop="start_serial_no" :rules="[{ required: true, message: '' }]" class="start-serial-no">
-        <el-input v-model="form.start_serial_no" :placeholder="$t('请输入开始序列号')" class="max-w400" />
+      <el-form-item for="no_click" :label="$t('起始流水号')" prop="start_serial_no" :rules="[{ required: true, message: '' }]" class="start-serial-no">
+        <el-input v-model="form.start_serial_no" :placeholder="$t('请输入起始流水号')" class="max-w400" />
       </el-form-item>
     </el-form>
     <!--提交-->
