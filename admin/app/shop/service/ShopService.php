bc59823cd46a8611857cc5be023e82f3a6f4c002
王昱 <nightcp874@gmail.com>
2025-09-01 14:37:55 +0800

    fix: 2.5.13-商家后台-接口报错

--- admin/app/shop/service/ShopService.php
+++ admin/app/shop/service/ShopService.php
@@ -89,7 +89,7 @@ class ShopService
                 // 实收金额
                 'total_money' => $businessData['total_received_price'],
                 // 折扣总额
-                'total_discount_money' => Helper::bcadd($businessData['total_discount_money'], $businessData['total_user_discount_money']),
+                'total_discount_money' => Helper::bcadd($businessData['total_discount_money'] ?: 0, $businessData['total_user_discount_money'] ?: 0),
                 // 优惠折扣
                 'discount_money' => $businessData['total_discount_money'],
                 // 会员折扣
