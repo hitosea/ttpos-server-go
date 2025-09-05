17e01342c35da25ecae6421f4cdd94247534463c
rikugun <v5.rikugun@gmail.com>
2025-09-05 10:53:49 +0800

    feat: 新增初始化数据库时可以修改默认数据库root用户名

--- ttpos-bmp/hack/init_db.sh
+++ ttpos-bmp/hack/init_db.sh
@@ -13,7 +13,13 @@ fi
 
 . "$ENV_FILE"
 
-export EXEC="mysql -h $DB_HOST -uroot -p$DB_ROOT_PASSWORD -P $DB_PORT_OPEN"
+# 判断DB_ROOT_USERNAME是否为空，如果为空则设置为root
+if [ -z "$DB_ROOT_USERNAME" ]; then
+  DB_ROOT_USERNAME="root"
+fi
+
+export EXEC="mysql -h $DB_HOST -u$DB_ROOT_USERNAME -p$DB_ROOT_PASSWORD -P $DB_PORT_OPEN"
+
 docker run --rm mariadb:10.11.6 $EXEC -e "CREATE DATABASE IF NOT EXISTS erp DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
 docker run --rm mariadb:10.11.6 $EXEC -e "CREATE DATABASE IF NOT EXISTS takeout DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
 docker run --rm mariadb:10.11.6 $EXEC -e "GRANT ALL PRIVILEGES ON erp.* TO '$DB_USERNAME'@'%'  WITH GRANT OPTION;"
