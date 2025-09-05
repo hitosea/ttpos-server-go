a30ad918275aa89f0cf33e8a5270d8fac41f4046
hexiang <869411657@qq.com>
2025-09-05 09:15:44 +0800

    fix:  版本号

--- admin/views/admin/config.js
+++ admin/views/admin/config.js
@@ -4,7 +4,7 @@ window.config = () => {
     brand: 'TTPOS', // 地区 日本=> jbc, 泰国=>TTPOS
     webLogo: '', // webLogo有值是优先加载Logo 否则按照地区加载默认图标
     build: 76, // 版本build
-    versionName: '2.3.0', // 版本号
+    versionName: '2.5.0', // 版本号
     debug: true, // 是否显示vconsole
     pageSize: 10, // 缓存商品的分页条数设置
   };
