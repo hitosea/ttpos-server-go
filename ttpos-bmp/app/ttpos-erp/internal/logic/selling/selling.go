8ca77f99169df9a65d57cedd64062917629b345f
rikugun <v5.rikugun@gmail.com>
2025-09-28 08:31:55 +0800

    feat: 增加重复发票提交的约束

--- ttpos-bmp/app/ttpos-erp/internal/logic/selling/selling.go
+++ ttpos-bmp/app/ttpos-erp/internal/logic/selling/selling.go
@@ -379,12 +379,12 @@ func (s *sSelling) parsePosProfileResponse(resp *g.Var) (*erp.POSProfile, error)
 func (s *sSelling) OpenPosEntry(ctx context.Context, req *selling.OpenPosEntryReq) (*selling.OpenPosEntryResp, error) {
 	// 检查POS配置文件是否已开帐
 	// opening, err := s.IsProfileOpening(ctx, req.PosProfileName, req.CashierEmail)
-	if err != nil {
-		return nil, gerror.Wrapf(err, "查询POS配置文件是否开帐失败")
-	}
+	//if err != nil {
+	//	return nil, gerror.Wrapf(err, "查询POS配置文件是否开帐失败")
+	//}
 	//新的异步模式下，不再约束已开账检查
 	// if opening {
-		// return nil, gerror.New("POS配置文件已开帐")
+	// return nil, gerror.New("POS配置文件已开帐")
 	// }
 
 	// 获取公司名称
