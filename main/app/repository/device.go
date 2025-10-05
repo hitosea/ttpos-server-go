7b2490f6f4dea4f5f59077ae6ae02e250938d48c
曾振华 <zhenhua.zeng@qq.com>
2025-10-05 23:25:54 +0800

    fix: 限制一台设备登录时，重复登录同一个deviceId报超限

--- main/app/repository/device.go
+++ main/app/repository/device.go
@@ -13,17 +13,17 @@ type IDeviceRepo interface {
 	WhereSource(source string) DBOption // 来源（cashier、tablet、kitchen、assistant）条件
 	WhereMain() DBOption                // 主设备条件
 
-	GetDevice(opts ...DBOption) (model.Device, error)       // 获取设备 - 不包含软删除
-	GetDeviceAll(opts ...DBOption) (model.Device, error)    // 获取设备 - 所有 - 包含软删除
-	GetDeviceBySn(sn string) (*model.Device, error)         // 根据sn获取设备
-	GetDeviceByUuid(uuid uint64) (*model.Device, error)     // 根据uuid获取设备
-	GetDeviceBrand(opts ...DBOption) string                 // 获取设备品牌
-	GetDeviceSn(opts ...DBOption) string                    // 获取设备sn
-	GetBindCountBySource(source string) uint                // 根据来源获取设备绑定数量
-	IsExistCashierMain(source string) bool                  // 是否存在主设备
-	CreateDevice(device model.Device) (model.Device, error) // 创建设备
-	UpdateDevice(uuid uint64, vars map[string]any) error    // 更新设备
-	GetDeviceList(opts ...DBOption) ([]model.Device, error) // 获取设备列表
+	GetDevice(opts ...DBOption) (model.Device, error)        // 获取设备 - 不包含软删除
+	GetDeviceAll(opts ...DBOption) (model.Device, error)     // 获取设备 - 所有 - 包含软删除
+	GetDeviceBySn(sn string) (*model.Device, error)          // 根据sn获取设备
+	GetDeviceByUuid(uuid uint64) (*model.Device, error)      // 根据uuid获取设备
+	GetDeviceBrand(opts ...DBOption) string                  // 获取设备品牌
+	GetDeviceSn(opts ...DBOption) string                     // 获取设备sn
+	GetBindCountBySource(source, ignoreDeviceId string) uint // 根据来源获取设备绑定数量
+	IsExistCashierMain(source string) bool                   // 是否存在主设备
+	CreateDevice(device model.Device) (model.Device, error)  // 创建设备
+	UpdateDevice(uuid uint64, vars map[string]any) error     // 更新设备
+	GetDeviceList(opts ...DBOption) ([]model.Device, error)  // 获取设备列表
 
 }
 
@@ -118,9 +118,9 @@ func (r *deviceRepo) WhereUuid(uuid uint64) DBOption {
 	}
 }
 
-func (r *deviceRepo) GetBindCountBySource(source string) uint {
+func (r *deviceRepo) GetBindCountBySource(source, ignoreDeviceId string) uint {
 	var count int64
-	r.db.Model(&model.Device{}).Scopes(NotDeleted).Where("source = ? AND finally_login_uuid > 0", source).Count(&count)
+	r.db.Model(&model.Device{}).Scopes(NotDeleted).Where("source = ? AND finally_login_uuid > 0 AND device_id != ?", source, ignoreDeviceId).Count(&count)
 	return uint(count)
 }
 
