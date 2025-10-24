058593e60f4f00f9b4a8793f538584fd6eaafcaa
xiezhihuan <602666178@qq.com>
2025-10-24 16:20:04 +0800

    fix: 修复获取班次失败

--- main/app/service/order_action.go
+++ main/app/service/order_action.go
@@ -234,7 +234,7 @@ func (s *orderSrv) ActionCooking(ctx context.Context, ignoreMust bool, saleBill 
 		if staffUuid > 0 {
 			staffShiftLog, err := GetCurrentStaffShiftLog(db, staffUuid)
 			if err != nil {
-				return nil, errors.WithMessage(err, "GetCurrentStaffShiftLog failed")
+				logger.Logger.Error("获取当前未交班的班次列表失败", zap.Uint64("staffUuid", staffUuid), zap.Error(err))
 			} else {
 				staffShiftLogUuid = staffShiftLog.Uuid
 			}
