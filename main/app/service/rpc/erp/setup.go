1ac951a9b489a6bbae4f7860446d0ee942ad77d2
曾振华 <zhenhua.zeng@qq.com>
2025-10-28 18:02:46 +0800

    fix: ttpos 关联erp 时，判断已关联的erp company 增加  delete_time =0 的约束

--- main/app/service/rpc/erp/setup.go
+++ main/app/service/rpc/erp/setup.go
@@ -32,7 +32,7 @@ func (s *erpSrv) InitShop(ctx cc.Context, initShopReq req.InitShopReq) (resp.Ini
 	companySettingRepo := repository.NewCompanySettingRepo(s.dbm.GetDB(0))
 	// 判断是否已经有店铺
 	companySetting, _ := companySettingRepo.GetOne(companySettingRepo.WhereErpnextCompanyAbbr(initShopReq.CompanyAbbr), companySettingRepo.WhereSiteCode(initShopReq.SiteCode))
-	if companySetting.Uuid != 0 {
+	if companySetting.Uuid != 0 && companySetting.DeleteTime == 0 {
 		return resp.InitShopResp{}, errors.New("所属erpnext公司已授权其他商家")
 	}
 
