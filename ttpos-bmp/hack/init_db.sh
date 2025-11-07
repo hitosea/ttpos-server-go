f6372dc54aec4a0b306656edbf25963e05b3749c
rikugun <v5.rikugun@gmail.com>
2025-11-07 06:40:10 +0000

    feat: 调整初始化脚本创建数据库的字符集

--- ttpos-bmp/hack/init_db.sh
+++ ttpos-bmp/hack/init_db.sh
@@ -20,9 +20,9 @@ fi
 
 export EXEC="mysql -h $DB_HOST -u$DB_ROOT_USERNAME -p$DB_ROOT_PASSWORD -P $DB_PORT_OPEN"
 
-docker run --rm mariadb:10.11.6 $EXEC -e "CREATE DATABASE IF NOT EXISTS erp DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
-docker run --rm mariadb:10.11.6 $EXEC -e "CREATE DATABASE IF NOT EXISTS takeout DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
-docker run --rm mariadb:10.11.6 $EXEC -e "CREATE DATABASE IF NOT EXISTS message DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
+docker run --rm mariadb:10.11.6 $EXEC -e "CREATE DATABASE IF NOT EXISTS erp DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+docker run --rm mariadb:10.11.6 $EXEC -e "CREATE DATABASE IF NOT EXISTS takeout DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+docker run --rm mariadb:10.11.6 $EXEC -e "CREATE DATABASE IF NOT EXISTS message DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
 docker run --rm mariadb:10.11.6 $EXEC -e "GRANT ALL PRIVILEGES ON erp.* TO '$DB_USERNAME'@'%'  WITH GRANT OPTION;"
 docker run --rm mariadb:10.11.6 $EXEC -e "GRANT ALL PRIVILEGES ON takeout.* TO '$DB_USERNAME'@'%'  WITH GRANT OPTION;"
 docker run --rm mariadb:10.11.6 $EXEC -e "GRANT ALL PRIVILEGES ON message.* TO '$DB_USERNAME'@'%'  WITH GRANT OPTION;"
