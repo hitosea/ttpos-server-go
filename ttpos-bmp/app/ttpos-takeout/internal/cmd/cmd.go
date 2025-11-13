dfd52156977d2d8ac1050942224caab5ee651087
rikugun <v5.rikugun@gmail.com>
2025-11-13 11:00:12 +0000

    feat:  在调用链跟踪中增加grpc请求报文。

--- ttpos-bmp/app/ttpos-takeout/internal/cmd/cmd.go
+++ ttpos-bmp/app/ttpos-takeout/internal/cmd/cmd.go
@@ -1,30 +1,33 @@
 package cmd
 
 import (
-	"context"
-	"ttpos-bmp/app/ttpos-takeout/internal/controller/callback"
-	"ttpos-bmp/internal/pkg/middleware"
+    "context"
+    "ttpos-bmp/app/ttpos-takeout/internal/controller/callback"
+    "ttpos-bmp/internal/pkg/middleware"
 
-	"github.com/gogf/gf/v2/frame/g"
-	"github.com/gogf/gf/v2/net/ghttp"
-	"github.com/gogf/gf/v2/os/gcmd"
+    "github.com/gogf/gf/v2/frame/g"
+    "github.com/gogf/gf/v2/net/ghttp"
+    "github.com/gogf/gf/v2/os/gcmd"
 
-	"ttpos-bmp/app/ttpos-takeout/internal/controller/hello"
+    "ttpos-bmp/app/ttpos-takeout/internal/controller/hello"
+    "ttpos-bmp/internal/pkg/otlp"
 )
 
 var (
 	Main = gcmd.Command{
 		Name:  "main",
 		Usage: "main",
-		Brief: "start http server",
-		Func: func(ctx context.Context, parser *gcmd.Parser) (err error) {
+        Brief: "start http server",
+        Func: func(ctx context.Context, parser *gcmd.Parser) (err error) {
 
-			//rpc.InitRpc(ctx)
+            //rpc.InitRpc(ctx)
+            shutdownOtlp := otlp.InitOtlp()
+            defer shutdownOtlp(ctx)
 
-			s := g.Server()
-			s.Group("/", func(group *ghttp.RouterGroup) {
-				group.Middleware(ghttp.MiddlewareHandlerResponse,
-					middleware.MiddlewareHandlerVersion)
+            s := g.Server()
+            s.Group("/", func(group *ghttp.RouterGroup) {
+                group.Middleware(ghttp.MiddlewareHandlerResponse,
+                    middleware.MiddlewareHandlerVersion)
 				group.Bind(
 					hello.NewV1(),
 					callback.NewV1(),
