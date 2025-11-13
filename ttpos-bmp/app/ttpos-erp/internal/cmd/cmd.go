bea5335591925fbfa697945e5bbfd54aa67c8867
rikugun <v5.rikugun@gmail.com>
2025-11-13 03:28:23 +0000

    feat: 增加批量迁移ERPNEXT 指令支持

--- ttpos-bmp/app/ttpos-erp/internal/cmd/cmd.go
+++ ttpos-bmp/app/ttpos-erp/internal/cmd/cmd.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"sort"
 	"ttpos-bmp/app/ttpos-erp/internal/consts"
 	"ttpos-bmp/app/ttpos-erp/internal/controller/callback"
 	"ttpos-bmp/internal/pkg/otlp"
@@ -65,8 +67,46 @@ var (
 			return nil
 		},
 	}
-)
 
-type cMigrate struct {
-	g.Meta `name:"migrate" brief:"执行ERP数据迁移"`
-}
+	ErpAllMigrate = &gcmd.Command{
+		Name:  "migrate-all",
+		Usage: "migrate-all --siteCode 1 --dirBase ./manifest/erp-migrate",
+		Brief: "遍历所有版本目录并执行ERP数据迁移",
+		Func: func(ctx context.Context, parser *gcmd.Parser) (err error) {
+			g.Log().Info(ctx, "开始执行ERP全量迁移...")
+
+			siteCode := parser.GetOpt("siteCode", "1").String()
+			baseDir := parser.GetOpt("dirBase", "./manifest/erp-migrate").String()
+			ctx = grpcx.Ctx.NewIncoming(ctx, g.Map{
+				consts.ContextSiteCode: siteCode,
+			})
+
+			entries, readErr := os.ReadDir(baseDir)
+			if readErr != nil {
+				g.Log().Error(ctx, "读取迁移目录失败", readErr)
+				return readErr
+			}
+
+			var dirs []string
+			for _, e := range entries {
+				if e.IsDir() {
+					dirs = append(dirs, e.Name())
+				}
+			}
+			sort.Strings(dirs)
+
+			for _, d := range dirs {
+				dirPath := fmt.Sprintf("%s/%s", baseDir, d)
+				g.Log().Infof(ctx, "执行版本目录: %s", dirPath)
+				if err := service.Setup().InitErpDocTypeWithDirname(ctx, dirPath); err != nil {
+					g.Log().Errorf(ctx, "迁移失败: %s", err)
+					return err
+				}
+			}
+
+			g.Log().Info(ctx, "ERP全量迁移执行完成!")
+			os.Exit(0)
+			return nil
+		},
+	}
+)
