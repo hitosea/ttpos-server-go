a4518fe29448a620ba185d985b94408741e9e54b
xiezhihuan <602666178@qq.com>
2025-11-17 20:30:43 +0800

    fix: 修复制作完成后关闭智能厨显不显示制作时间

--- main/app/service/business.go
+++ main/app/service/business.go
@@ -1086,12 +1086,17 @@ func (s *businessSrv) KitchenProductionDetail(ctx context.Context, req req.Kitch
 			continue // 跳过套餐商品
 		}
 		item := business_data_resp.KitchenProductionDetailItem{
-			ProductName:    productionOrderProduct.SaleOrderProduct.MultiLanguageName.GetNames(),
-			FlavorName:     productionOrderProduct.SaleOrderProduct.GetFlavorName(),
-			CategoryName:   productionOrderProduct.ProductCategory.MultiLanguageName.GetNames(),
-			Number:         productionOrderProduct.Num,
-			CreateTime:     productionOrderProduct.CreateTime,
-			MakeFinishTime: productionOrderProduct.MadeTime,
+			ProductName:  productionOrderProduct.SaleOrderProduct.MultiLanguageName.GetNames(),
+			FlavorName:   productionOrderProduct.SaleOrderProduct.GetFlavorName(),
+			CategoryName: productionOrderProduct.ProductCategory.MultiLanguageName.GetNames(),
+			Number:       productionOrderProduct.Num,
+			CreateTime:   productionOrderProduct.CreateTime,
+			MakeFinishTime: func() int64 {
+				if productionOrderProduct.SendDuration == 0 { // 如果上菜时间大于0,则返回0,表示关闭了智能后厨
+					return 0
+				}
+				return productionOrderProduct.MadeTime
+			}(),
 			MakeDuration: func() int64 {
 				if productionOrderProduct.SendDuration == 0 { // 如果上菜时间大于0,则返回0,表示关闭了智能后厨
 					return 0
