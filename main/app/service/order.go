675a3a67c4b36f8552b994f5c164022ce680dda6
xiezhihuan <602666178@qq.com>
2025-12-01 11:26:24 +0800

    fix: 修复加价的定义错误,加价表示套餐中每份加价的金额,而不是每个商品加价的金额

--- main/app/service/order.go
+++ main/app/service/order.go
@@ -1928,10 +1928,10 @@ func (s *orderSrv) newSaleOrderProduct(ctx context.Context, params CreateSaleOrd
 				params.SaleOrder.SaleOrderProducts = append(params.SaleOrder.SaleOrderProducts, subProducts...)
 				saleOrderProducts = append(saleOrderProducts, subProducts...)
 
-				// 计算套餐主商品的加价总和 = Σ(子商品加价 × 子商品数量)
+				// 计算套餐主商品的加价总和 = Σ(子商品每份的加价 × 子商品份数)
 				totalAddPrice := decimal.NewFromFloat(0.0)
 				for _, subProduct := range subProducts {
-					totalAddPrice = totalAddPrice.Add(decimal.NewFromFloat(subProduct.AddPrice).Mul(decimal.NewFromFloat(subProduct.Num)))
+					totalAddPrice = totalAddPrice.Add(decimal.NewFromFloat(subProduct.AddPrice).Mul(decimal.NewFromFloat(subProduct.CopyNum)))
 				}
 				saleOrderProduct.AddPrice = totalAddPrice.InexactFloat64()
 			}
@@ -2010,10 +2010,10 @@ func (s *orderSrv) newSaleOrderProduct(ctx context.Context, params CreateSaleOrd
 					params.SaleOrder.SaleOrderProducts = append(params.SaleOrder.SaleOrderProducts, subProducts...)
 					saleOrderProducts = append(saleOrderProducts, subProducts...)
 
-					// 计算套餐主商品的加价总和 = Σ(子商品加价 × 子商品数量)
+					// 计算套餐主商品的加价总和 = Σ(子商品每份加价 × 子商品份数)
 					totalAddPrice := decimal.NewFromFloat(0.0)
 					for _, subProduct := range subProducts {
-						totalAddPrice = totalAddPrice.Add(decimal.NewFromFloat(subProduct.AddPrice).Mul(decimal.NewFromFloat(subProduct.Num)))
+						totalAddPrice = totalAddPrice.Add(decimal.NewFromFloat(subProduct.AddPrice).Mul(decimal.NewFromFloat(subProduct.CopyNum)))
 					}
 					saleOrderProduct.AddPrice = totalAddPrice.InexactFloat64()
 				}
