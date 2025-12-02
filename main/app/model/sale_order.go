d4c17fcc770ade0e17e480be7b1fede08e40c18a
xiezhihuan <602666178@qq.com>
2025-12-02 11:12:49 +0800

    fix: 修复平板送厨商品的套餐子商品单位数量

--- main/app/model/sale_order.go
+++ main/app/model/sale_order.go
@@ -791,7 +791,7 @@ func (model *SaleOrder) GetProductList(hasOrderedH5ProductWithReject bool, openI
 					LocaleName:          subProduct.MultiLanguageName.GetNames(),
 					LocaleAttributeName: subProduct.GetAttributeName(),
 					Num:                 subProduct.Num,
-					UnitNum:             subProduct.GetProductNum(),
+					UnitNum:             subProduct.GetUnitNum(),
 					AddPrice:            subProduct.AddPrice, // 子商品加价金额
 				})
 			}
