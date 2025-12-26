a09e69d54c97663be13889d7a985b158f243c83d
rikugun <v5.rikugun@gmail.com>
2025-12-26 07:54:25 +0000

    refactor: update field types from interface{} to any in multiple DAO models for improved type safety and clarity

--- ttpos-bmp/app/ttpos-erp/internal/model/do/warehouse_logistics.go
+++ ttpos-bmp/app/ttpos-erp/internal/model/do/warehouse_logistics.go
@@ -11,9 +11,9 @@ import (
 // WarehouseLogistics is the golang structure of table erp_warehouse_logistics for DAO operations like Where/Data.
 type WarehouseLogistics struct {
 	g.Meta        `orm:"table:erp_warehouse_logistics, do:true"`
-	Id            interface{} // ID
-	SiteCode      interface{} // 站点编码。 关联 erp_site.site_code
-	ShopUuid      interface{} // ttpos商铺ID
-	WarehouseCode interface{} // 仓库编码. erpnext warehouse
-	LogisticsId   interface{} // 物流ID
+	Id            any // ID
+	SiteCode      any // 站点编码。 关联 erp_site.site_code
+	ShopUuid      any // ttpos商铺ID
+	WarehouseCode any // 仓库编码. erpnext warehouse
+	LogisticsId   any // 物流ID
 }
