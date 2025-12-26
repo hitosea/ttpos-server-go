e4d7362a80567fd8e11f3ca196f53360580217fe
wfs <605403358@qq.com>
2025-12-26 07:28:08 +0000

    feat: 外卖平台默认关闭

--- main/app/modules/takeout/domain/model/takeout.go
+++ main/app/modules/takeout/domain/model/takeout.go
@@ -4,7 +4,7 @@ package model
 type Takeout struct {
 	BaseModel
 	Platform    string      `gorm:"column:platform;type:varchar(50);comment:外卖平台(grab/lineman等);NOT NULL" json:"platform"`
-	Enabled     bool        `gorm:"column:enabled;type:tinyint(1);default:1;comment:是否开启(1:开启 0:关闭);NOT NULL" json:"enabled"`
+	Enabled     bool        `gorm:"column:enabled;type:tinyint(1);default:0;comment:是否开启(1:开启 0:关闭);NOT NULL" json:"enabled"`
 	IsBound     bool        `gorm:"column:is_bound;type:tinyint(1);default:0;comment:是否已经绑定平台(1:已绑定 0:未绑定);NOT NULL" json:"is_bound"`
 	Skip        bool        `gorm:"column:skip;type:tinyint(1);default:0;comment:是否跳过绑定(1:跳过 0:不跳过);NOT NULL" json:"skip"`
 	BindingLink string      `gorm:"column:binding_link;type:varchar(500);default:'';comment:平台绑定链接(缓存用);NOT NULL" json:"binding_link"`
