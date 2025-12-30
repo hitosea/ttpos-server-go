e6a2cac51ff49a4e223723e5982b05575dba0156
wfs <605403358@qq.com>
2025-12-30 17:31:38 +0000

    fix: 修复交班退款金额的显示

--- main/app/service/statistics.go
+++ main/app/service/statistics.go
@@ -2098,6 +2098,7 @@ func (s *statisticsSrv) CountShiftRefundAmount(ctx context.Context, req CountReq
 	opts := []repository.DBOption{
 		commonRepo.WhereByDutyNo(req.DutyNo),
 		commonRepo.WhereBySoftDelete(),
+		returnOrderRepo.WhereNotReverseSettlement(),
 	}
 	if req.ExcludeDataManage {
 		saleOrderUuids := []uint64{}
