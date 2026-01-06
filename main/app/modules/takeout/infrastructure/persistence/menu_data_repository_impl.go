bc985ff53fa44b0e18fa2eea9d41b2f18370c272
wfs <605403358@qq.com>
2026-01-06 11:37:54 +0000

    fix: TTPOS 删除商品后，Grab下单标记异常

--- main/app/modules/takeout/infrastructure/persistence/menu_data_repository_impl.go
+++ main/app/modules/takeout/infrastructure/persistence/menu_data_repository_impl.go
@@ -408,10 +408,10 @@ func (r *menuDataRepositoryImpl) GetProductNamesByUuids(ctx context.Context, pro
 			}
 		}
 
-		// 如果外卖表没有，回退到核心表
-		if displayName == "" {
-			displayName = ttposName
-		}
+		// // 如果外卖表没有，回退到核心表
+		// if displayName == "" {
+		// 	displayName = ttposName
+		// }
 
 		// ErpCode: 从ProductBom获取
 		if bom, ok := bomMap[productUuid]; ok {
@@ -658,9 +658,9 @@ func (r *menuDataRepositoryImpl) GetModifierNamesByUuids(ctx context.Context, mo
 				}
 
 				// 如果外卖表没有，回退到核心表
-				if displayName == "" {
-					displayName = ttposName
-				}
+				// if displayName == "" {
+				// 	displayName = ttposName
+				// }
 
 				// 获取规格信息和ERP编码
 				if pkg.ProductBomUuid > 0 {
