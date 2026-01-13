394044dd1f30545257baf4accd6b0b8b47f7665c
hexiang <869411657@qq.com>
2026-01-13 15:05:15 +0800

    fix: 优化-商家后台-套餐 - 添加分组商品- 选择商品窗口loading中，应置灰搜索框

--- admin/views/shop/src/components/product/Selector.vue
+++ admin/views/shop/src/components/product/Selector.vue
@@ -80,10 +80,10 @@
         <div class="product-selector-form">
           <el-form size="small" ref="formRef" :model="form" :inline="true">
             <el-form-item :label="$t('商品名称')" :placeholder="$t('请输入商品名称')">
-              <el-input size="small" v-model="form.product_name" @input="onDebounceSearch" />
+              <el-input size="small" v-model="form.product_name" @input="onDebounceSearch" :disabled="loading" />
             </el-form-item>
             <el-form-item>
-              <el-button size="small" type="primary" icon="Search" class="search-button" @click="onSearch">
+              <el-button size="small" type="primary" icon="Search" class="search-button" @click="onSearch" :disabled="loading">
                 {{ $t('查询') }}
               </el-button>
             </el-form-item>
@@ -629,6 +629,7 @@
 
   let searchTimer;
   const onDebounceSearch = () => {
+    if (loading.value) return;
     clearTimeout(searchTimer);
     searchTimer = setTimeout(() => {
       onSearch();
@@ -638,6 +639,7 @@
   const searchValue = ref('');
 
   const onSearch = () => {
+    if (loading.value) return;
     searchValue.value = form.product_name;
 
     nextTick(() => {
