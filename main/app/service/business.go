50ed8bad42a290f8bd67cbd635cfed0f2ce16d2c
王昱 <nightcp874@gmail.com>
2026-01-27 02:23:51 +0000

    fix(service): 修复渠道营业统计导出表格最后一行缺失边框

--- main/app/service/business.go
+++ main/app/service/business.go
@@ -3149,8 +3149,8 @@ func (s *businessSrv) ExportChannelSalesTask(ctx context.Context, params ExportC
 			"dine_in_store":    "堂食",
 			"takeaway":         "外带",
 			"grab":             "Grab",
-			"lineman":          "LINE MAN",
-			"takeout":          "外卖",
+			// "lineman":          "LINE MAN",
+			"takeout": "外卖",
 		},
 		"en": {
 			"summary":          "Total",
@@ -3173,8 +3173,8 @@ func (s *businessSrv) ExportChannelSalesTask(ctx context.Context, params ExportC
 			"dine_in_store":    "สั่งอาหารในร้าน",
 			"takeaway":         "นำอาหารกลับบ้าน",
 			"grab":             "Grab",
-			"lineman":          "LINE MAN",
-			"takeout":          "สั่งอาหารกลับบ้าน",
+			// "lineman":          "LINE MAN",
+			"takeout": "สั่งอาหารกลับบ้าน",
 		},
 		"zhtw": {
 			"summary":          "合計",
@@ -3185,8 +3185,8 @@ func (s *businessSrv) ExportChannelSalesTask(ctx context.Context, params ExportC
 			"dine_in_store":    "店內點餐",
 			"takeaway":         "外帶",
 			"grab":             "Grab",
-			"lineman":          "LINE MAN",
-			"takeout":          "外送",
+			// "lineman":          "LINE MAN",
+			"takeout": "外送",
 		},
 		"ja": {
 			"summary":          "合計",
@@ -3197,8 +3197,8 @@ func (s *businessSrv) ExportChannelSalesTask(ctx context.Context, params ExportC
 			"dine_in_store":    "店内点餐",
 			"takeaway":         "外帶",
 			"grab":             "Grab",
-			"lineman":          "LINE MAN",
-			"takeout":          "外送",
+			// "lineman":          "LINE MAN",
+			"takeout": "外送",
 		},
 		"ko": {
 			"summary":          "합계",
@@ -3209,8 +3209,8 @@ func (s *businessSrv) ExportChannelSalesTask(ctx context.Context, params ExportC
 			"dine_in_store":    "店内点餐",
 			"takeaway":         "外帶",
 			"grab":             "Grab",
-			"lineman":          "LINE MAN",
-			"takeout":          "外送",
+			// "lineman":          "LINE MAN",
+			"takeout": "外送",
 		},
 		"my": {
 			"summary":          "စုစုပေါင်း",
@@ -3221,8 +3221,8 @@ func (s *businessSrv) ExportChannelSalesTask(ctx context.Context, params ExportC
 			"dine_in_store":    "ဆိုင်တွင်မှာယူ",
 			"takeaway":         "အိမ်သို့ယူ",
 			"grab":             "Grab",
-			"lineman":          "LINE MAN",
-			"takeout":          "အိမ်သို့ယူ",
+			// "lineman":          "LINE MAN",
+			"takeout": "အိမ်သို့ယူ",
 		},
 		"tr": {
 			"summary":          "Toplam",
@@ -3233,8 +3233,8 @@ func (s *businessSrv) ExportChannelSalesTask(ctx context.Context, params ExportC
 			"dine_in_store":    "Restoranda Sipariş",
 			"takeaway":         "Paket Sipariş",
 			"grab":             "Grab",
-			"lineman":          "LINE MAN",
-			"takeout":          "Teslimat",
+			// "lineman":          "LINE MAN",
+			"takeout": "Teslimat",
 		},
 		"sv": {
 			"summary":          "Totalt",
@@ -3245,8 +3245,8 @@ func (s *businessSrv) ExportChannelSalesTask(ctx context.Context, params ExportC
 			"dine_in_store":    "Restoranda Sipariş",
 			"takeaway":         "Paket Sipariş",
 			"grab":             "Grab",
-			"lineman":          "LINE MAN",
-			"takeout":          "Teslimat",
+			// "lineman":          "LINE MAN",
+			"takeout": "Teslimat",
 		},
 	}
 	channelNames := channelNameMap[lang]
@@ -3449,7 +3449,7 @@ func (s *businessSrv) ExportChannelSalesTask(ctx context.Context, params ExportC
 	_, sheet1EndRow := writeChannelData(sheet1Name, &rowIdx1, channelNames["takeout"], params.Result.Takeout, false)
 
 	// 为 Sheet1 设置样式和列宽
-	for row := 1; row < sheet1EndRow; row++ {
+	for row := 1; row <= sheet1EndRow; row++ {
 		cellA, _ := excelize.CoordinatesToCellName(1, row)
 		xlsxFile.SetCellStyle(sheet1Name, cellA, cellA, aColumnStyle)
 		cellB, _ := excelize.CoordinatesToCellName(2, row)
@@ -3475,11 +3475,11 @@ func (s *businessSrv) ExportChannelSalesTask(ctx context.Context, params ExportC
 	writeChannelData(sheet2Name, &rowIdx2, channelNames["dine_in"], params.Result.DineIn, false)
 	writeChannelData(sheet2Name, &rowIdx2, channelNames["takeout_shop"], params.Result.TakeoutShop, false)
 	writeChannelData(sheet2Name, &rowIdx2, channelNames["takeout_delivery"], params.Result.TakeoutDelivery, false)
-	writeChannelData(sheet2Name, &rowIdx2, channelNames["grab"], params.Result.Grab, false)
-	_, sheet2EndRow := writeChannelData(sheet2Name, &rowIdx2, channelNames["lineman"], params.Result.Lineman, false)
+	_, sheet2EndRow := writeChannelData(sheet2Name, &rowIdx2, channelNames["grab"], params.Result.Grab, false)
+	// _, sheet2EndRow := writeChannelData(sheet2Name, &rowIdx2, channelNames["lineman"], params.Result.Lineman, false)
 
 	// 为 Sheet2 设置样式和列宽
-	for row := 1; row < sheet2EndRow; row++ {
+	for row := 1; row <= sheet2EndRow; row++ {
 		cellA, _ := excelize.CoordinatesToCellName(1, row)
 		xlsxFile.SetCellStyle(sheet2Name, cellA, cellA, aColumnStyle)
 		cellB, _ := excelize.CoordinatesToCellName(2, row)
