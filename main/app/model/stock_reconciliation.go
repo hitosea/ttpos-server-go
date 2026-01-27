99d57bdbc4a6025ae56c2b0e0fcba3a6b5c948f6
xiezhihuan <602666178@qq.com>
2026-01-27 03:52:18 +0000

    fix: 删除无用字段

--- main/app/model/stock_reconciliation.go
+++ main/app/model/stock_reconciliation.go
@@ -14,8 +14,7 @@ type StockReconciliation struct {
 	Purpose            int    `gorm:"column:purpose;not null;default:1;comment:盘点目的 1-库存盘点 2-期初盘点" json:"purpose"`
 	Status             int    `gorm:"column:status;not null;default:0;index:idx_status;comment:状态 0-已保存 1-已提交 2-已审核 3-已驳回" json:"status"`
 	SubmitTime         int    `gorm:"column:submit_time;not null;default:0;comment:提交时间(时间戳)" json:"submit_time"`
-	CreatorStaffUuid   uint64 `gorm:"column:creator_staff_uuid;not null;default:0;comment:发起人员工UUID" json:"creator_staff_uuid"`
-	SubmitterStaffUuid uint64 `gorm:"column:submitter_staff_uuid;not null;default:0;comment:提交人员工UUID" json:"submitter_staff_uuid"`
+	SubmitterStaffUuid uint64 `gorm:"column:submitter_staff_uuid;not null;default:0;comment:发起人员工UUID" json:"submitter_staff_uuid"`
 
 	// 关联仓库
 	Warehouse *Warehouse `gorm:"foreignKey:WarehouseUuid;references:Uuid"`
