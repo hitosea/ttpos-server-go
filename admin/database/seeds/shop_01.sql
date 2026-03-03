b5346c12f52dac13bfb3aa5cbf88a94086586a8b
wfs <605403358@qq.com>
2026-03-03 02:08:09 +0000

    feat: 添加索引

--- admin/database/seeds/shop_01.sql
+++ admin/database/seeds/shop_01.sql
@@ -2184,7 +2184,9 @@ CREATE TABLE IF NOT EXISTS `ttpos_warehouse_out_form_item` (
     INDEX `idx_warehouse_out_form_uuid` (`warehouse_out_form_uuid`),
     INDEX `idx_material_uuid` (`material_uuid`),
     INDEX `idx_product_bom_uuid` (`product_bom_uuid`),
-    INDEX `idx_sale_bill_uuid` (`sale_bill_uuid`)
+    INDEX `idx_sale_bill_uuid` (`sale_bill_uuid`),
+    INDEX `idx_staff_shift_log_uuid` (`staff_shift_log_uuid`),
+    INDEX `idx_sale_order_uuid` (`sale_order_uuid`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '出库单明细表';
 
 CREATE TABLE IF NOT EXISTS `ttpos_loss_report_form` (
