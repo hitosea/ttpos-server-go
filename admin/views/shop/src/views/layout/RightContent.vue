3daf3b649f2b0cfd2e00ac94b86fcad8ba7b2dad
hexiang <869411657@qq.com>
2026-03-17 17:04:11 +0800

    feat(settings): 添加settings数据支持并显示营业状态提示

--- admin/views/shop/src/views/layout/RightContent.vue
+++ admin/views/shop/src/views/layout/RightContent.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="right-content pr">
+    <div class="right-content-top" v-if="business_status == 1">
+      {{ $t('当前门店营业状态为：“测试营业”，门店订单数据不计入汇总统计，仅用于内部测试。若确认正式营业请到“商家档案”板块进行开启。') }}
+    </div>
     <!--内容区域-->
     <div class="right-content-box">
       <div class="subject-wrap">
@@ -15,10 +18,30 @@
 <script setup>
   import ChildTabs from '@/views/layout/childTabs.vue';
   import { useRoute } from 'vue-router';
+  import { useUserStore } from '@/store';
+
   const route = useRoute();
   const emit = defineEmits(['selectMenu']);
+  const { computedSettings } = useUserStore();
+  const settings = computedSettings().settings;
+  const business_status = settings?.value?.business_status || 1;
+
+  console.log(business_status);
 
   const selectMenu = (e) => {
     emit('selectMenu', e);
   };
 </script>
+<style scoped lang="scss">
+  .right-content-top {
+    display: flex;
+    padding: 12px 16px;
+    justify-content: center;
+    align-items: center;
+    gap: 24px;
+    align-self: stretch;
+    background: #ffeee0;
+    font-size: 14px;
+    color: #ff6600;
+  }
+</style>
