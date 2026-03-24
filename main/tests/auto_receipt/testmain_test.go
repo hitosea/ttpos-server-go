fef1f1b66abaa8436e42497e7d89405395ec8f9a
HeWeiTao <675428202@qq.com>
2026-03-24 10:45:32 +0000

    chore(test): drop auto receipt tenant DBs eagerly

--- main/tests/auto_receipt/testmain_test.go
+++ main/tests/auto_receipt/testmain_test.go
@@ -12,6 +12,11 @@ import (
 )
 
 func TestMain(m *testing.M) {
+	// auto_receipt tests are synchronous request/response flows. Drop tenant DBs
+	// as soon as each test finishes so this heavy suite does not accumulate full
+	// cloned schemas and exhaust the MySQL tmpfs used in CI.
+	_ = os.Setenv("TEST_DROP_TENANT_DB", "1")
+
 	if os.Getenv("TEST_ENABLE_RESOURCE_BUDGET") != "1" {
 		os.Exit(m.Run())
 	}
